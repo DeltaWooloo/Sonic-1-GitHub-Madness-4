a3f91a2d9820c9e07d557eba88957783501fa71a
GuykeAto89 <95724820+Guyke89@users.noreply.github.com>
2026-03-07 23:45:24 -0300

    mario

--- _maps/Moto Bug.asm
+++ _maps/Moto Bug.asm
@@ -1,50 +1,26 @@
-; ---------------------------------------------------------------------------
-; Sprite mappings - Moto Bug enemy (GHZ)
-; ---------------------------------------------------------------------------
-Map_Moto_internal:	mappingsTable
-	mappingsTableEntry.w	.moto1
-	mappingsTableEntry.w	.moto2
-	mappingsTableEntry.w	.moto3
-	mappingsTableEntry.w	.smoke1
-	mappingsTableEntry.w	.smoke2
-	mappingsTableEntry.w	.smoke3
-	mappingsTableEntry.w	.blank
+; --------------------------------------------------------------------------------
+; Sprite mappings - output from SonMapEd - Sonic 1 format
+; --------------------------------------------------------------------------------
 
-.moto1:	spriteHeader
-	spritePiece	-$14, -$10, 4, 2, 0, 0, 0, 0, 0
-	spritePiece	-$14, 0, 4, 1, 8, 0, 0, 0, 0
-	spritePiece	$C, -8, 1, 2, $C, 0, 0, 0, 0
-	spritePiece	-$C, 8, 3, 1, $E, 0, 0, 0, 0
-.moto1_End
-
-.moto2:	spriteHeader
-	spritePiece	-$14, -$F, 4, 2, 0, 0, 0, 0, 0
-	spritePiece	-$14, 1, 4, 1, 8, 0, 0, 0, 0
-	spritePiece	$C, -7, 1, 2, $C, 0, 0, 0, 0
-	spritePiece	-$C, 9, 3, 1, $11, 0, 0, 0, 0
-.moto2_End
-
-.moto3:	spriteHeader
-	spritePiece	-$14, -$10, 4, 2, 0, 0, 0, 0, 0
-	spritePiece	-$14, 0, 4, 1, $14, 0, 0, 0, 0
-	spritePiece	$C, -8, 1, 2, $C, 0, 0, 0, 0
-	spritePiece	-$14, 8, 2, 1, $18, 0, 0, 0, 0
-	spritePiece	-4, 8, 2, 1, $12, 0, 0, 0, 0
-.moto3_End
-
-.smoke1:	spriteHeader
-	spritePiece	$10, -6, 1, 1, $1A, 0, 0, 0, 0
-.smoke1_End
-
-.smoke2:	spriteHeader
-	spritePiece	$10, -6, 1, 1, $1B, 0, 0, 0, 0
-.smoke2_End
-
-.smoke3:	spriteHeader
-	spritePiece	$10, -6, 1, 1, $1C, 0, 0, 0, 0
-.smoke3_End
-
-.blank:	spriteHeader
-.blank_End
-
-	even
+SME_abo6T:	
+		dc.w SME_abo6T_E-SME_abo6T, SME_abo6T_19-SME_abo6T	
+		dc.w SME_abo6T_24-SME_abo6T, SME_abo6T_2F-SME_abo6T	
+		dc.w SME_abo6T_35-SME_abo6T, SME_abo6T_3B-SME_abo6T	
+		dc.w SME_abo6T_41-SME_abo6T	
+SME_abo6T_E:	dc.b 2	
+		dc.b $C8, $F, 0, 0, $EC	
+		dc.b $E8, $F, 0, $10, $EC	
+SME_abo6T_19:	dc.b 2	
+		dc.b $C0, $F, 0, 0, $EC	
+		dc.b $E0, $F, 0, $10, $EC	
+SME_abo6T_24:	dc.b 2	
+		dc.b $C8, $F, 0, 0, $EC	
+		dc.b $E8, $F, 0, $10, $EC	
+SME_abo6T_2F:	dc.b 1	
+		dc.b $FA, 0, 0, $1A, $10	
+SME_abo6T_35:	dc.b 1	
+		dc.b $FA, 0, 0, $1B, $10	
+SME_abo6T_3B:	dc.b 1	
+		dc.b $FA, 0, 0, $28, $10	
+SME_abo6T_41:	dc.b 0	
+		even
