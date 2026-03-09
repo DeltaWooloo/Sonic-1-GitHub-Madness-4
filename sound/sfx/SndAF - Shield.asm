11d96a668afda85d483ffa3725e4c7bb01fca4bb
GuykeAto89 <95724820+Guyke89@users.noreply.github.com>
2026-03-09 13:59:03 -0300

    Shield sfx

--- sound/sfx/SndAF - Shield.asm
+++ sound/sfx/SndAF - Shield.asm
@@ -4,12 +4,12 @@ SndAF_Shield_Header:
 	smpsHeaderTempoSFX  $01
 	smpsHeaderChanSFX   $01
 
-	smpsHeaderSFXChannel cFM5, SndAF_Shield_FM5,	$0C, $00
+	smpsHeaderSFXChannel cFM5, SndAF_Shield_FM5,	$0A, $00
 
 ; FM5 Data
 SndAF_Shield_FM5:
 	smpsSetvoice        $00
-	dc.b	nRst, $01, nBb2, $05, smpsNoAttack, nB2, $26
+	dc.b	nRst, $04, nBb2, $0A, smpsNoAttack, nB2, $26
 	smpsStop
 
 SndAF_Shield_Voices:
@@ -17,17 +17,18 @@ SndAF_Shield_Voices:
 ;	$30
 ;	$30, $30, $30, $30, 	$9E, $A8, $AC, $DC, 	$0E, $0A, $04, $05
 ;	$08, $08, $08, $08, 	$BF, $BF, $BF, $BF, 	$04, $2C, $14, $80
-	smpsVcAlgorithm     $00
-	smpsVcFeedback      $06
-	smpsVcUnusedBits    $00
-	smpsVcDetune        $03, $03, $03, $03
-	smpsVcCoarseFreq    $00, $00, $00, $00
-	smpsVcRateScale     $03, $02, $02, $02
-	smpsVcAttackRate    $1C, $2C, $28, $1E
+	smpsVcAlgorithm     $02
+	smpsVcFeedback      $07
+	smpsVcUnusedBits    $03
+	smpsVcDetune        $03, $01, $03, $02
+	smpsVcCoarseFreq    $02, $00, $00, $01
+	smpsVcRateScale     $00, $00, $00, $00
+	smpsVcAttackRate    $2F, $2F, $1F, $2F
 	smpsVcAmpMod        $00, $00, $00, $00
-	smpsVcDecayRate1    $05, $04, $0A, $0E
-	smpsVcDecayRate2    $08, $08, $08, $08
-	smpsVcDecayLevel    $0B, $0B, $0B, $0B
+	smpsVcDecayRate1    $02, $09, $08, $05
+	smpsVcDecayRate2    $02, $06, $0F, $06
+	smpsVcDecayLevel    $02, $04, $02, $01
 	smpsVcReleaseRate   $0F, $0F, $0F, $0F
-	smpsVcTotalLevel    $00, $14, $2C, $04
+	smpsVcTotalLevel    $00, $0E, $1A, $0F
+
 
