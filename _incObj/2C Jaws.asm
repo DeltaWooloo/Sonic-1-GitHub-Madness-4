14d5bf20e51bf844dbeec10f97b1ed2861ba5187
talkwalker <60858748+ToxicWater-Johan-bbbbooooiiiiiiiiii12@users.noreply.github.com>
2026-03-09 11:08:30 -0500

    behavior
    g

--- _incObj/2C Jaws.asm
+++ _incObj/2C Jaws.asm
@@ -25,7 +25,7 @@ Jaws_Main:	; Routine 0
 		move.b	#$10,obActWid(a0)
 		moveq	#0,d0
 		move.b	obSubtype(a0),d0 ; load object subtype number
-		lsl.w	#8,d0		; multiply d0 by 64
+		lsl.w	#1,d0		; multiply d0 by 64
 		subq.w	#1,d0
 		move.w	d0,jaws_timecount(a0) ; set turn delay time
 		move.w	d0,jaws_timedelay(a0)
@@ -35,6 +35,7 @@ Jaws_Main:	; Routine 0
 		neg.w	obVelX(a0)	; move Jaws to the right
 
 Jaws_Turn:	; Routine 2
+        addi.w	#$20,obVelX(a0)	
 		subq.w	#1,jaws_timecount(a0) ; subtract 1 from turn delay time
 		bpl.s	.animate	; if time remains, branch
 		move.w	jaws_timedelay(a0),jaws_timecount(a0) ; reset turn delay time
