20c72355bd27022c35d1a1ed60ec21b9af24fd32
HS/C <carlos.edu.iagnecz@gmail.com>
2026-03-12 14:18:13 -0300

    OHSHIT

--- build.lua
+++ build.lua
@@ -35,7 +35,7 @@ if abort then
 end
 
 print("Making release")
-if is_windows() then
+if common.is_windows() then
     os.execute("copy gm4built.gen __Release\\GHM4.gen")
 else
     print("Fuck you, no release rom for you in specific!!")
