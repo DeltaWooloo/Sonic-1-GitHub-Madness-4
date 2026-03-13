3df0ba62c542673d8e60f595d18909042c6f6a14
DaxKatter2117 <daxternator2117@gmail.com>
2026-03-13 20:17:41 +0800

    Whoops

--- sound/s1.sounddriver.asm
+++ sound/s1.sounddriver.asm
@@ -264,11 +264,6 @@ ptr_mus51:		dc.l Music51
 ;ptr_mus54:		dc.l Music54
 ;ptr_mus55:		dc.l Music55
 ptr_mus56:		dc.l Music56
-;ptr_mus52:		dc.l Music52
-;ptr_mus53:		dc.l Music53
-;ptr_mus54:		dc.l Music54
-;ptr_mus55:		dc.l Music55
-;ptr_mus56:		dc.l Music56
 ;ptr_mus57:		dc.l Music57
 ;ptr_mus58:		dc.l Music58
 ;ptr_mus59:		dc.l Music59
