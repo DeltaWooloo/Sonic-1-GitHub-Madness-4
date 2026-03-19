53dba9b50e8a53a46808122ead592005bc3b3248
ConiNight <129713751+ConiNight@users.noreply.github.com>
2026-03-20 02:16:25 +0300

    Banana Bro Len art and maps

---
+++ coniobjs/IZMap.asm
@@ -0,0 +1,125 @@
+Map_6db3: mappingsTable
+	mappingsTableEntry.w	Map_6db3_0
+	mappingsTableEntry.w	Map_6db3_1
+	mappingsTableEntry.w	Map_6db3_2
+	mappingsTableEntry.w	Map_6db3_3
+	mappingsTableEntry.w	Map_6db3_4
+	mappingsTableEntry.w	Map_6db3_5
+	mappingsTableEntry.w	Map_6db3_6
+	mappingsTableEntry.w	Map_6db3_7
+	mappingsTableEntry.w	Map_6db3_8
+	mappingsTableEntry.w	Map_6db3_9
+	mappingsTableEntry.w	Map_6db3_10
+	mappingsTableEntry.w	Map_6db3_11
+	mappingsTableEntry.w	Map_6db3_12
+	mappingsTableEntry.w	Map_6db3_13
+	mappingsTableEntry.w	Map_6db3_14
+	mappingsTableEntry.w	Map_6db3_15
+
+Map_6db3_0:	spriteHeader
+ spritePiece 0, 4, 2, 3, $4D, 0, 0, 0, 0
+ spritePiece -$10, -$1C, 4, 4, 0, 0, 0, 0, 0
+ spritePiece $10, -$17, 1, 1, $10, 0, 0, 0, 0
+ spritePiece -$10, 4, 4, 1, $11, 0, 0, 0, 0
+ spritePiece -$10, 4, 2, 3, $4D, 1, 0, 0, 0
+ spritePiece -7, 4, 2, 3, $4D, 0, 0, 0, 0
+Map_6db3_0_End
+
+Map_6db3_1:	spriteHeader
+ spritePiece 2, 4, 3, 2, $53, 0, 0, 0, 0
+ spritePiece -$10, -$1C, 4, 4, 0, 0, 0, 0, 0
+ spritePiece $10, -$17, 1, 1, $10, 0, 0, 0, 0
+ spritePiece -$10, 4, 4, 1, $11, 0, 0, 0, 0
+ spritePiece -$10, 4, 2, 3, $4D, 1, 0, 0, 0
+ spritePiece -7, 4, 2, 3, $4D, 0, 0, 0, 0
+Map_6db3_1_End
+
+Map_6db3_2:	spriteHeader
+ spritePiece 0, 4, 2, 3, $4D, 0, 0, 0, 0
+ spritePiece -$10, -$1C, 4, 4, 0, 0, 0, 0, 0
+ spritePiece $10, -$17, 1, 1, $10, 0, 0, 0, 0
+ spritePiece -$10, 4, 4, 1, $11, 0, 0, 0, 0
+ spritePiece -$1A, 4, 3, 2, $53, 1, 0, 0, 0
+ spritePiece -7, 4, 2, 3, $4D, 0, 0, 0, 0
+Map_6db3_2_End
+
+Map_6db3_3:	spriteHeader
+ spritePiece 0, 4, 2, 3, $4D, 0, 0, 0, 0
+ spritePiece -$10, -$1C, 4, 4, 0, 0, 0, 0, 0
+ spritePiece $10, -$17, 1, 1, $10, 0, 0, 0, 0
+ spritePiece -$10, 4, 4, 1, $11, 0, 0, 0, 0
+ spritePiece -$10, 4, 2, 3, $4D, 1, 0, 0, 0
+ spritePiece -8, 3, 2, 2, $55, 0, 0, 0, 0
+Map_6db3_3_End
+
+Map_6db3_4:	spriteHeader
+ spritePiece 0, 4, 2, 3, $4D, 0, 0, 0, 0
+ spritePiece -$10, -$1C, 4, 4, $28, 0, 0, 0, 0
+ spritePiece $10, -$1C, 1, 2, $38, 0, 0, 0, 0
+ spritePiece -$10, 4, 4, 1, $11, 0, 0, 0, 0
+ spritePiece -$10, 4, 2, 3, $4D, 1, 0, 0, 0
+ spritePiece -7, 4, 2, 3, $4D, 0, 0, 0, 0
+Map_6db3_4_End
+
+Map_6db3_5:	spriteHeader
+ spritePiece 0, 4, 2, 3, $4D, 0, 0, 0, 0
+ spritePiece -$10, -$1C, 4, 4, $3D, 0, 0, 0, 0
+ spritePiece -$18, -$1C, 1, 3, $3A, 0, 0, 0, 0
+ spritePiece -$10, 4, 4, 1, $11, 0, 0, 0, 0
+ spritePiece -$10, 4, 2, 3, $4D, 1, 0, 0, 0
+ spritePiece -7, 4, 2, 3, $4D, 0, 0, 0, 0
+Map_6db3_5_End
+
+Map_6db3_6:	spriteHeader
+ spritePiece 2, 4, 3, 2, $53, 0, 0, 0, 0
+ spritePiece -$10, -$1C, 4, 4, 0, 0, 0, 0, 0
+ spritePiece $10, -$17, 1, 1, $10, 0, 0, 0, 0
+ spritePiece -$10, 4, 4, 1, $11, 0, 0, 0, 0
+ spritePiece -$1A, 4, 3, 2, $53, 1, 0, 0, 0
+ spritePiece -8, 3, 2, 2, $55, 0, 0, 0, 0
+Map_6db3_6_End
+
+Map_6db3_7:	spriteHeader
+ spritePiece 0, -$24, 1, 1, $15, 0, 0, 0, 0
+ spritePiece -$18, -$14, 1, 2, $16, 0, 0, 0, 0
+ spritePiece 2, 4, 3, 2, $53, 0, 0, 0, 0
+ spritePiece -$10, -$1C, 4, 4, $18, 0, 0, 0, 0
+ spritePiece $10, -$18, 1, 1, $10, 0, 0, 0, 0
+ spritePiece -$10, 4, 4, 1, $11, 0, 0, 0, 0
+ spritePiece -$1A, 4, 3, 2, $53, 1, 0, 0, 0
+ spritePiece -8, 3, 2, 2, $55, 0, 0, 0, 0
+Map_6db3_7_End
+
+Map_6db3_8:	spriteHeader
+ spritePiece -8, -4, 2, 1, $5F, 0, 0, 0, 0
+Map_6db3_8_End
+
+Map_6db3_9:	spriteHeader
+ spritePiece -8, -8, 2, 2, $5B, 0, 1, 0, 0
+Map_6db3_9_End
+
+Map_6db3_10:	spriteHeader
+ spritePiece -4, -8, 1, 2, $59, 0, 1, 0, 0
+Map_6db3_10_End
+
+Map_6db3_11:	spriteHeader
+ spritePiece -8, -8, 2, 2, $5B, 1, 1, 0, 0
+Map_6db3_11_End
+
+Map_6db3_12:	spriteHeader
+ spritePiece -8, -4, 2, 1, $5F, 1, 0, 0, 0
+Map_6db3_12_End
+
+Map_6db3_13:	spriteHeader
+ spritePiece -8, -8, 2, 2, $5B, 1, 0, 0, 0
+Map_6db3_13_End
+
+Map_6db3_14:	spriteHeader
+ spritePiece -4, -8, 1, 2, $59, 0, 0, 0, 0
+Map_6db3_14_End
+
+Map_6db3_15:	spriteHeader
+ spritePiece -8, -8, 2, 2, $5B, 0, 0, 0, 0
+Map_6db3_15_End
+
+	even
---
+++ coniobjs/LenMap.asm
@@ -0,0 +1,92 @@
+Map_b3f0: mappingsTable
+	mappingsTableEntry.w	Map_b3f0_0
+	mappingsTableEntry.w	Map_b3f0_1
+	mappingsTableEntry.w	Map_b3f0_2
+	mappingsTableEntry.w	Map_b3f0_3
+	mappingsTableEntry.w	Map_b3f0_4
+	mappingsTableEntry.w	Map_b3f0_5
+	mappingsTableEntry.w	Map_b3f0_6
+	mappingsTableEntry.w	Map_b3f0_7
+	mappingsTableEntry.w	Map_b3f0_8
+	mappingsTableEntry.w	Map_b3f0_9
+	mappingsTableEntry.w	Map_b3f0_10
+	mappingsTableEntry.w	Map_b3f0_11
+	mappingsTableEntry.w	Map_b3f0_12
+	mappingsTableEntry.w	Map_b3f0_13
+
+Map_b3f0_0:	spriteHeader
+ spritePiece -3, -$E, 2, 2, $2E, 0, 0, 0, 0
+ spritePiece -6, -$1B, 2, 2, 0, 0, 0, 0, 0
+ spritePiece -$F, -$E, 3, 2, $10, 0, 0, 0, 0
+ spritePiece -$E, 2, 3, 4, $16, 0, 0, 0, 0
+Map_b3f0_0_End
+
+Map_b3f0_1:	spriteHeader
+ spritePiece -3, -$E, 2, 2, $2E, 0, 0, 0, 0
+ spritePiece -6, -$1B, 2, 2, 4, 0, 0, 0, 0
+ spritePiece -$F, -$E, 3, 2, $10, 0, 0, 0, 0
+ spritePiece -$E, 2, 3, 4, $22, 0, 0, 0, 0
+Map_b3f0_1_End
+
+Map_b3f0_2:	spriteHeader
+ spritePiece -3, -$E, 2, 2, $2E, 0, 0, 0, 0
+ spritePiece -6, -$1B, 2, 2, 8, 0, 0, 0, 0
+ spritePiece -$F, -$E, 3, 2, $10, 0, 0, 0, 0
+ spritePiece -$E, 2, 3, 4, $16, 0, 0, 0, 0
+Map_b3f0_2_End
+
+Map_b3f0_3:	spriteHeader
+ spritePiece -3, -$E, 2, 2, $2E, 0, 0, 0, 0
+ spritePiece -6, -$1B, 2, 2, $C, 0, 0, 0, 0
+ spritePiece -$F, -$E, 3, 2, $10, 0, 0, 0, 0
+ spritePiece -$E, 2, 3, 4, $22, 0, 0, 0, 0
+Map_b3f0_3_End
+
+Map_b3f0_4:	spriteHeader
+ spritePiece 4, -$18, 2, 2, $32, 0, 0, 0, 0
+ spritePiece -6, -$1B, 2, 2, $C, 0, 0, 0, 0
+ spritePiece -$F, -$E, 3, 2, $10, 0, 0, 0, 0
+ spritePiece -$E, 2, 3, 4, $16, 0, 0, 0, 0
+ spritePiece -1, -$1E, 4, 3, $52, 0, 0, 1, 0
+Map_b3f0_4_End
+
+Map_b3f0_5:	spriteHeader
+ spritePiece -8, -$18, 2, 2, $36, 0, 0, 0, 0
+ spritePiece -6, -$1B, 2, 2, $C, 0, 0, 0, 0
+ spritePiece -$F, -$E, 3, 2, $10, 0, 0, 0, 0
+ spritePiece -$E, 2, 3, 4, $16, 0, 0, 0, 0
+Map_b3f0_5_End
+
+Map_b3f0_6:	spriteHeader
+ spritePiece -$C, -$10, 3, 4, $3A, 0, 0, 1, 0
+Map_b3f0_6_End
+
+Map_b3f0_7:	spriteHeader
+ spritePiece -$C, -$10, 3, 4, $46, 0, 0, 1, 0
+Map_b3f0_7_End
+
+Map_b3f0_8:	spriteHeader
+ spritePiece -$10, -$C, 4, 3, $52, 0, 0, 1, 0
+Map_b3f0_8_End
+
+Map_b3f0_9:	spriteHeader
+ spritePiece -$10, -$C, 4, 3, $5E, 0, 0, 1, 0
+Map_b3f0_9_End
+
+Map_b3f0_10:	spriteHeader
+ spritePiece -$C, -$10, 3, 4, $3A, 1, 1, 1, 0
+Map_b3f0_10_End
+
+Map_b3f0_11:	spriteHeader
+ spritePiece -$C, -$10, 3, 4, $46, 1, 1, 1, 0
+Map_b3f0_11_End
+
+Map_b3f0_12:	spriteHeader
+ spritePiece -$10, -$C, 4, 3, $52, 1, 1, 1, 0
+Map_b3f0_12_End
+
+Map_b3f0_13:	spriteHeader
+ spritePiece -$10, -$C, 4, 3, $5E, 1, 1, 1, 0
+Map_b3f0_13_End
+
+	even
