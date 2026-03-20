6f3ead72ba6f96b4d8d9007b498759c1b47537f4
ConiNight <129713751+ConiNight@users.noreply.github.com>
2026-03-20 03:38:21 +0300

    Spongy Art

---
+++ coniobjs/SpongyMap.asm
@@ -0,0 +1,75 @@
+Map_0216: mappingsTable
+	mappingsTableEntry.w	Map_0216_0
+	mappingsTableEntry.w	Map_0216_1
+	mappingsTableEntry.w	Map_0216_2
+	mappingsTableEntry.w	Map_0216_3
+	mappingsTableEntry.w	Map_0216_4
+	mappingsTableEntry.w	Map_0216_5
+	mappingsTableEntry.w	Map_0216_6
+	mappingsTableEntry.w	Map_0216_7
+	mappingsTableEntry.w	Map_0216_8
+
+Map_0216_0:	spriteHeader
+ spritePiece -$10, -$18, 4, 2, 0, 0, 0, 0, 0
+ spritePiece -$10, -8, 4, 4, 8, 0, 0, 0, 0
+ spritePiece -$18, 8, 2, 2, $47, 0, 0, 1, 0
+Map_0216_0_End
+
+Map_0216_1:	spriteHeader
+ spritePiece -$18, 8, 2, 2, $47, 0, 0, 1, 0
+ spritePiece -$C, -$10, 3, 1, $1C, 0, 0, 0, 0
+ spritePiece -$14, -8, 4, 4, $1F, 0, 0, 0, 0
+ spritePiece -$1C, -$18, 2, 2, $18, 0, 0, 0, 0
+ spritePiece -$1C, -8, 1, 1, $2F, 0, 0, 0, 0
+Map_0216_1_End
+
+Map_0216_2:	spriteHeader
+ spritePiece -$18, 8, 2, 2, $47, 0, 0, 1, 0
+ spritePiece -$C, -$18, 3, 1, $1C, 0, 0, 0, 0
+ spritePiece -$14, -$10, 4, 4, $1F, 0, 0, 0, 0
+ spritePiece -$1C, -$20, 2, 2, $18, 0, 0, 0, 0
+ spritePiece -$1C, -$10, 1, 1, $2F, 0, 0, 0, 0
+ spritePiece -$18, 1, 2, 1, $44, 0, 0, 0, 0
+Map_0216_2_End
+
+Map_0216_3:	spriteHeader
+ spritePiece -$10, -$20, 4, 2, 0, 0, 0, 0, 0
+ spritePiece -$10, -$10, 4, 4, $30, 0, 0, 0, 0
+ spritePiece -$18, -$10, 1, 1, $40, 0, 0, 0, 0
+ spritePiece $10, -8, 1, 2, $41, 0, 0, 0, 0
+ spritePiece -$18, 8, 2, 2, $47, 0, 0, 1, 0
+Map_0216_3_End
+
+Map_0216_4:	spriteHeader
+ spritePiece -$10, -$18, 4, 2, 0, 0, 0, 0, 0
+ spritePiece -$10, -8, 4, 4, $30, 0, 0, 0, 0
+ spritePiece -$18, -8, 1, 1, $40, 0, 0, 0, 0
+ spritePiece $10, 0, 1, 2, $41, 0, 0, 0, 0
+ spritePiece -$18, 8, 2, 2, $47, 0, 0, 1, 0
+Map_0216_4_End
+
+Map_0216_5:	spriteHeader
+ spritePiece -$10, -$10, 2, 2, $43, 0, 0, 0, 0
+ spritePiece -$10, 0, 2, 2, $43, 0, 1, 0, 1
+ spritePiece 0, 0, 2, 2, $43, 1, 1, 0, 0
+Map_0216_5_End
+
+Map_0216_6:	spriteHeader
+ spritePiece -$10, -$10, 2, 2, $43, 0, 0, 0, 0
+ spritePiece 0, -$10, 2, 2, $43, 1, 0, 0, 0
+ spritePiece -$10, 0, 2, 2, $43, 0, 1, 0, 1
+Map_0216_6_End
+
+Map_0216_7:	spriteHeader
+ spritePiece -$10, -$10, 2, 2, $43, 0, 0, 0, 0
+ spritePiece 0, -$10, 2, 2, $43, 1, 0, 0, 0
+ spritePiece 0, 0, 2, 2, $43, 1, 1, 0, 0
+Map_0216_7_End
+
+Map_0216_8:	spriteHeader
+ spritePiece 0, -$10, 2, 2, $43, 1, 0, 0, 0
+ spritePiece -$10, 0, 2, 2, $43, 0, 1, 0, 1
+ spritePiece 0, 0, 2, 2, $43, 1, 1, 0, 0
+Map_0216_8_End
+
+	even
