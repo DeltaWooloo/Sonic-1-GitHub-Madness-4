38ed078f0bbacdf2985890089f18124869cb0103
dawid from da 32bit <tetakkrme6@gmail.com>
2026-04-03 22:46:32 +0200

    le fixen

--- _maps/Mouse.asm
+++ _maps/Mouse.asm
@@ -1,22 +1,20 @@
-Map_48ce:
-Map_48ce_0: 	dc.w Map_48ce_E-Map_48ce
-Map_48ce_2: 	dc.w Map_48ce_19-Map_48ce
-Map_48ce_4: 	dc.w Map_48ce_24-Map_48ce
-Map_48ce_6: 	dc.w Map_48ce_2F-Map_48ce
-Map_48ce_8: 	dc.w Map_48ce_30-Map_48ce
-Map_48ce_A: 	dc.w Map_48ce_31-Map_48ce
-Map_48ce_C: 	dc.w Map_48ce_32-Map_48ce
-Map_48ce_E: 	dc.b $2
-	dc.b $EC, $2, $0, $0, $F0
-	dc.b $F4, $B, $0, $3, $F8
-Map_48ce_19: 	dc.b $2
-	dc.b $EC, $2, $0, $0, $F0
-	dc.b $F4, $B, $0, $3, $F8
-Map_48ce_24: 	dc.b $2
-	dc.b $EC, $2, $0, $0, $F0
-	dc.b $F4, $B, $0, $3, $F8
-Map_48ce_2F: 	dc.b $0
-Map_48ce_30: 	dc.b $0
-Map_48ce_31: 	dc.b $0
-Map_48ce_32: 	dc.b $0
+Map_8bf5:
+Map_8bf5_0: 	dc.w Map_8bf5_E-Map_8bf5
+Map_8bf5_2: 	dc.w Map_8bf5_19-Map_8bf5
+Map_8bf5_4: 	dc.w Map_8bf5_1F-Map_8bf5
+Map_8bf5_6: 	dc.w Map_8bf5_25-Map_8bf5
+Map_8bf5_8: 	dc.w Map_8bf5_26-Map_8bf5
+Map_8bf5_A: 	dc.w Map_8bf5_27-Map_8bf5
+Map_8bf5_C: 	dc.w Map_8bf5_28-Map_8bf5
+Map_8bf5_E: 	dc.b $2
+	dc.b $F0, $2, $0, $0, $F8
+	dc.b $F8, $2, $0, $3, $0
+Map_8bf5_19: 	dc.b $1
+	dc.b $F4, $A, $0, $6, $F4
+Map_8bf5_1F: 	dc.b $1
+	dc.b $F4, $A, $0, $6, $F4
+Map_8bf5_25: 	dc.b $0
+Map_8bf5_26: 	dc.b $0
+Map_8bf5_27: 	dc.b $0
+Map_8bf5_28: 	dc.b $0
 	even
