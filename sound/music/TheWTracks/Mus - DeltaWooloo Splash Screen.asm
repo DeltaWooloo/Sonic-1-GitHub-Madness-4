86a33b57ae288768c90b9b25f9de991769b976b0
DaxKatter2117 <daxternator2117@gmail.com>
2026-04-05 22:47:31 +0800

    The W Splash Optimisation

--- sound/music/TheWTracks/Mus - DeltaWooloo Splash Screen.asm
+++ sound/music/TheWTracks/Mus - DeltaWooloo Splash Screen.asm
@@ -1,77 +1,44 @@
 SplashScreen_Header:
-	smpsHeaderStartSong 3
-	smpsHeaderVoice     SplashScreen_Voices
-	smpsHeaderChan      $06, $03
-	smpsHeaderTempo     $01, $68
+	smpsHeaderStartSong	3
+	smpsHeaderVoice		SplashScreen_Voices
+	smpsHeaderChan		$06, $03
+	smpsHeaderTempo		$01, $68
 
-	smpsHeaderDAC       SplashScreen_DAC
-	smpsHeaderFM        SplashScreen_FM1,	$00, $10
-	smpsHeaderFM        SplashScreen_FM2,	$00, $10
-	smpsHeaderFM        SplashScreen_FM3,	$00, $10
-	smpsHeaderFM        SplashScreen_FM4,	$00, $10
-	smpsHeaderFM        SplashScreen_FM5,	$00, $10
-	smpsHeaderPSG       SplashScreen_PSG1,	$F4, $09, $00, $00
-	smpsHeaderPSG       SplashScreen_PSG2,	$F4, $09, $00, $00
-	smpsHeaderPSG       SplashScreen_PSG3,	$F4, $09, $00, $00
-
-; FM1 Data
-SplashScreen_FM1:
-	smpsPan             panRight, $00
-	smpsSetvoice        $00
-	dc.b	nC4, $60, smpsNoAttack
-	smpsJump            SplashScreen_Jump00
-
-SplashScreen_Jump00:
-	dc.b	smpsNoAttack, $03
-	smpsFMAlterVol      $01
-	smpsLoop            $00, $38, SplashScreen_Jump00
-	smpsStop
+	smpsHeaderDAC		SplashScreen_DAC
+	smpsHeaderFM		SplashScreen_FM_R, $00, $10
+	smpsHeaderFM		SplashScreen_FM_C, $04, $10
+	smpsHeaderFM		SplashScreen_FM_L, $07, $10
+	smpsHeaderFM		SplashScreen_FM_C, $0B, $10
+	smpsHeaderFM		SplashScreen_FM_C, $0E, $10
+	smpsHeaderPSG		SplashScreen_PSG,  $F1, $09, $00, staTone_0F
+	smpsHeaderPSG		SplashScreen_PSG,  $F4, $09, $00, staTone_0F
+	smpsHeaderPSG		SplashScreen_PSG,  $FB, $09, $00, staTone_0F
 
 ; FM2 Data
-SplashScreen_FM2:
-	smpsPan             panCenter, $00
-	smpsSetvoice        $00
-	dc.b	nE4, $60, smpsNoAttack
-	smpsJump            SplashScreen_Jump00
+SplashScreen_FM_C:
+	smpsPan		panCenter, $00
+	smpsJump	SplashScreen_FM_Jump
 
 ; FM3 Data
-SplashScreen_FM3:
-	smpsPan             panLeft, $00
-	smpsSetvoice        $00
-	dc.b	nG4, $60, smpsNoAttack
-	smpsJump            SplashScreen_Jump00
+SplashScreen_FM_L:
+	smpsPan		panLeft, $00
+	smpsJump	SplashScreen_FM_Jump
 
-; FM4 Data
-SplashScreen_FM4:
-	smpsPan             panCenter, $00
-	smpsSetvoice        $00
-	dc.b	nB4, $60, smpsNoAttack
-	smpsJump            SplashScreen_Jump00
+; FM1 Data
+SplashScreen_FM_R:
+	smpsPan		panRight, $00
 
-; FM5 Data
-SplashScreen_FM5:
-	smpsPan             panCenter, $00
-	smpsSetvoice        $00
-	dc.b	nD5, $60, smpsNoAttack
-	smpsJump            SplashScreen_Jump00
+SplashScreen_FM_Jump:
+	smpsSetvoice	$00
 
-; PSG1 Data
-SplashScreen_PSG1:
-	smpsPSGvoice        staTone_0F
-	dc.b	nA3, $60, smpsNoAttack
-	smpsJump            SplashScreen_Jump00
-
-; PSG2 Data
-SplashScreen_PSG2:
-	smpsPSGvoice        staTone_0F
+SplashScreen_PSG:
 	dc.b	nC4, $60, smpsNoAttack
-	smpsJump            SplashScreen_Jump00
 
-; PSG3 Data
-SplashScreen_PSG3:
-	smpsPSGvoice        staTone_0F
-	dc.b	nG4, $60, smpsNoAttack
-	smpsJump            SplashScreen_Jump00
+SplashScreen_Loop00:
+	dc.b	smpsNoAttack, $03
+	smpsFMAlterVol		$01
+	smpsLoop		$00, $38, SplashScreen_Loop00
+	smpsStop
 
 ; DAC Data
 SplashScreen_DAC:
