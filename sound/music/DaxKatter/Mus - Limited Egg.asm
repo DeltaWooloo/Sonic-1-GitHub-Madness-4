1a5768592cf5a830c02095e699f51edb7fe6946a
DaxKatter2117 <daxternator2117@gmail.com>
2026-04-08 16:32:06 +0800

    Update Too LimitedSonic

--- sound/music/DaxKatter/Mus - Limited Egg.asm
+++ sound/music/DaxKatter/Mus - Limited Egg.asm
@@ -1,202 +1,219 @@
 BGM_LimitEgg_Header:
-	smpsHeaderStartSong 2
-	smpsHeaderVoice     BGM_LimitEgg_Voices
-	smpsHeaderChan      $06, $03
-	smpsHeaderTempo     $01, $60
+	smpsHeaderStartSong	2
+	smpsHeaderVoice		BGM_LimitEgg_Voices
+	smpsHeaderChan		$06, $03
+	smpsHeaderTempo		$01, $60
 
-	smpsHeaderDAC       BGM_LimitEgg_DAC
-	smpsHeaderFM        BGM_LimitEgg_FM1,	$00, $16
-	smpsHeaderFM        BGM_LimitEgg_FM2,	$00, $08
-	smpsHeaderFM        BGM_LimitEgg_FM3,	$00, $10
-	smpsHeaderFM        BGM_LimitEgg_FM4,	$00, $10
-	smpsHeaderFM        BGM_LimitEgg_FM5,	$00, $12
-	smpsHeaderPSG       BGM_LimitEgg_PSG1,	$E8, $06, $00, $00
-	smpsHeaderPSG       BGM_LimitEgg_PSG2,	$E8, $06, $00, $00
-	smpsHeaderPSG       BGM_LimitEgg_PSG3,	$00, $02, $00, fTone_01
+	smpsHeaderDAC		BGM_LimitEgg_DAC
+	smpsHeaderFM		BGM_LimitEgg_FM1,  $00, $16
+	smpsHeaderFM		BGM_LimitEgg_FM2,  $00, $08
+	smpsHeaderFM		BGM_LimitEgg_FM3,  $00, $10
+	smpsHeaderFM		BGM_LimitEgg_FM4,  $00, $10
+	smpsHeaderFM		BGM_LimitEgg_FM5,  $00, $12
+	smpsHeaderPSG		BGM_LimitEgg_PSG1, $E8, $06, $00, $00
+	smpsHeaderPSG		BGM_LimitEgg_PSG2, $E8, $06, $00, $00
+	smpsHeaderPSG		BGM_LimitEgg_PSG3, $00, $02, $00, fTone_01
 
 ; FM1 Data
 BGM_LimitEgg_FM1:
-	smpsSetvoice        $00
+	smpsSetvoice	$00
 
 BGM_LimitEgg_Jump01:
-	dc.b	nA5, $04, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5
-	dc.b	nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA6
-	dc.b	nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5
-	dc.b	nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5
-	dc.b	nA5, nA5, nA5, nA6, nA5, nA6, nA5, nA6, nA5, nA5, nA5, nA5
-	dc.b	nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5
-	dc.b	nA5, nA6, nA5, nA6, nA5, nA6, nA5, nA6, nA5, nA5, nA5, nA5
-	dc.b	nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA6
-	dc.b	nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5
-	dc.b	nA5, nA5, nA5, nA6, nA5, nA6, nA5, nA5, nA5, nA5, nA5, nA5
-	dc.b	nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5
-	dc.b	nA5, nA5, nA5, nA6, nA5, nA6, nA5, nA6, nA5, nA6, nA5, nA5
-	dc.b	nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5
-	dc.b	nA5, nA6, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA6, nA5, nA5
-	dc.b	nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5
-	dc.b	nA5, nA6, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5
-	dc.b	nA5, nA5, nA5, nA5, nA5, nA6, nA5, nA6, nA5, nA5, nA5, nA5
-	dc.b	nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5
-	dc.b	nA5, nA6, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5
-	dc.b	nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA6, nA5, nA5, nA5, nA5
-	dc.b	nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA6
-	dc.b	nA5, nA6, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5
-	dc.b	nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA6, nA5, nA5, nA5, nA5
-	dc.b	nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5, nA5
+	dc.b	nA5, $04
+	smpsLoop	0, 22, BGM_LimitEgg_Jump01
+	dc.b	nA6
+
+BGM_LimitEgg_Loop03:
 	dc.b	nA5
-	smpsJump            BGM_LimitEgg_Jump01
+	smpsLoop	0, 27, BGM_LimitEgg_Loop03
+	dc.b	nA6, nA5, nA6, nA5, nA6
+	smpsCall	BGM_LimitEgg_Call04
+	dc.b	nA6, nA5, nA6, nA5, nA6, nA5, nA6
+	smpsCall	BGM_LimitEgg_Call03
+	smpsCall	BGM_LimitEgg_Call03
+	dc.b	nA5, nA6
+
+BGM_LimitEgg_Loop06:
+	dc.b	nA5
+	smpsLoop	0, 21, BGM_LimitEgg_Loop06
+	dc.b	nA6, nA5, nA6, nA5, nA6, nA5, nA6
+	smpsCall	BGM_LimitEgg_Call03
+
+BGM_LimitEgg_Loop0B:
+	dc.b	nA5
+	smpsLoop	0, 7, BGM_LimitEgg_Loop0B
+	dc.b	nA6
+	smpsCall	BGM_LimitEgg_Call03
+	smpsCall	BGM_LimitEgg_Call03
+	dc.b	nA5, nA6
+
+BGM_LimitEgg_Loop0C:
+	smpsCall	BGM_LimitEgg_Call04
+	dc.b	nA6
+	smpsLoop	1, 2, BGM_LimitEgg_Loop0C
+	smpsCall	BGM_LimitEgg_Call03
+	dc.b	nA5, nA6
+	smpsCall	BGM_LimitEgg_Call04
+	dc.b	nA6
+	smpsCall	BGM_LimitEgg_Call04
+	smpsJump	BGM_LimitEgg_Jump01
+
+BGM_LimitEgg_Call03:
+	dc.b	nA5
+	smpsLoop	0, 15, BGM_LimitEgg_Call03
+	dc.b	nA6
+	smpsReturn
+
+BGM_LimitEgg_Call04:
+	dc.b	nA5
+	smpsLoop	0, 17, BGM_LimitEgg_Call04
+	smpsReturn
 
 ; FM3 Data
 BGM_LimitEgg_FM3:
-	smpsSetvoice        $02
-	smpsModSet          $08, $01, $05, $04
-
-BGM_LimitEgg_Loop06:
-	dc.b	nRst, $30
-
-BGM_LimitEgg_Loop05:
-	dc.b	nRst, $08, nEb6, $02, nEb6, nEb5, nRst, nEb6, nEb6, nEb5, nRst
-	smpsLoop            $01, $02, BGM_LimitEgg_Loop05
-	smpsLoop            $00, $02, BGM_LimitEgg_Loop06
-	smpsCall            BGM_LimitEgg_Call00
-	dc.b	nEb4, $10, nEb4, $08, nEb5, $10, nEb4, $08, nEb5, $0C, nEb5, $04
-	dc.b	nEb5, $08, nEb5, $10, nEb4, $08, nEb5, $10, nEb5, $04, nEb5, nEb5
-	dc.b	$10, nEb5, $04, nEb5, nEb5, $18, nEb5, $10, nEb5, $08, nEb4, $10
-	dc.b	nEb4, $08, nEb5, $10, nEb4, $08, nEb5, $0C, nEb5, $04, nEb5, $08
-	dc.b	nEb5, $10, nEb4, $08, nEb5, $60
+	smpsSetvoice	$02
+	smpsModSet	$08, $01, $05, $04
+	smpsCall	BGM_LimitEgg_Call00
+	smpsCall	BGM_LimitEgg_Call02
+	dc.b	nEb5, $10, $04, $04, $10, $04, $04, $18
+	dc.b	nEb5, $10, $08, nEb4, $10, $08, nEb5, $10, nEb4, $08, nEb5, $0C
+	dc.b	$04, $08, $10, nEb4, $08, nEb5, $60
 
 BGM_LimitEgg_Loop07:
-	dc.b	nEb6, $04, nEb6, nEb6, $10
-	smpsLoop            $00, $03, BGM_LimitEgg_Loop07
-	dc.b	nEb6, $04, nEb6, nEb6, $08, nEb5, nEb6, $30, nRst
-	smpsJump            BGM_LimitEgg_FM3
+	dc.b	nEb6, $04, $04, $10
+	smpsLoop	0, 3, BGM_LimitEgg_Loop07
+	dc.b	$04, $04, $08, nEb5, nEb6, $30, nRst
+	smpsJump	BGM_LimitEgg_FM3
 
 BGM_LimitEgg_Call00:
-	dc.b	nEb4, $10, nEb4, $08, nEb5, $10, nEb4, $08, nEb5, $0C, nEb5, $04
-	dc.b	nEb5, $08, nEb5, $10, nEb4, $08, nEb5, $18, nEb5, nEb5, nEb5, nEb4
-	dc.b	$10, nEb4, $08, nEb5, $10, nEb4, $08, nEb5, $0C, nEb5, $04, nEb5
-	dc.b	$08, nEb5, $10, nEb4, $08, nEb5, $20, nEb5, $08, nEb5, nEb5, $30
-	smpsReturn
-
-; FM4 Data
-BGM_LimitEgg_FM4:
-	smpsSetvoice        $02
-	smpsModSet          $08, $01, $05, $04
-
-BGM_LimitEgg_Loop03:
-	smpsAlterNote       $02
 	dc.b	nRst, $30
 
 BGM_LimitEgg_Loop02:
 	dc.b	nRst, $08, nEb6, $02, nEb6, nEb5, nRst, nEb6, nEb6, nEb5, nRst
-	smpsLoop            $01, $02, BGM_LimitEgg_Loop02
-	smpsLoop            $00, $02, BGM_LimitEgg_Loop03
-	smpsCall            BGM_LimitEgg_Call00
-	dc.b	nEb5, $10, nEb5, $08, nEb5, $10, nEb5, $08, nEb5, $0C, nEb5, $04
-	dc.b	nEb5, $08, nEb5, $10, nEb5, $08, nEb6, $10, nEb5, $04, nEb6, nEb5
-	dc.b	$10, nEb5, $04, nEb5, nEb5, $18, nEb5, $10, nEb5, $08, nEb5, $10
-	dc.b	nEb5, $08, nEb5, $10, nEb5, $08, nEb5, $0C, nEb5, $04, nEb5, $08
-	dc.b	nEb5, $10, nEb5, $08, nEb6, $60
+	smpsLoop	$01, $02, BGM_LimitEgg_Loop02
+	smpsLoop	$00, $02, BGM_LimitEgg_Call00
+	smpsCall	BGM_LimitEgg_Call01
+	smpsReturn
+
+BGM_LimitEgg_Call01:
+	smpsCall	BGM_LimitEgg_Call02
+	dc.b	nEb5, $18, nEb5, nEb5, nEb5
+	smpsCall	BGM_LimitEgg_Call02
+	dc.b	nEb5, $20, $08, $08, $30
+	smpsReturn
+
+BGM_LimitEgg_Call02:
+	dc.b	nEb4, $10, $08, nEb5, $10, nEb4, $08, nEb5, $0C, $04, $08, $10
+	dc.b	nEb4, $08
+	smpsReturn
+
+; FM4 Data
+BGM_LimitEgg_FM4:
+	smpsSetvoice	$02
+	smpsModSet	$08, $01, $05, $04
+	smpsAlterNote	$02
+	smpsCall	BGM_LimitEgg_Call00
+	dc.b	nEb5, $10, $08, $10, $08, $0C, $04, $08, $10, $08, nEb6, $10, nEb5, $04
+	dc.b	nEb6, nEb5, $10, $04, $04, $18
+	dc.b	$10, $08, $10, $08, $10, $08, $0C, $04, $08, $10, $08, nEb6, $60
 
 BGM_LimitEgg_Loop04:
 	dc.b	nEb6, $04, nEb5, nEb5, $10
-	smpsLoop            $00, $03, BGM_LimitEgg_Loop04
-	dc.b	nEb6, $04, nEb5, nEb5, $08, nEb5, nEb5, $30, nRst
-	smpsJump            BGM_LimitEgg_FM4
+	smpsLoop	0, 3, BGM_LimitEgg_Loop04
+	dc.b	nEb6, $04, nEb5, nEb5, $08, $08, $30, nRst
+	smpsJump	BGM_LimitEgg_FM4
 
 ; FM5 Data
 BGM_LimitEgg_FM5:
-	smpsSetvoice        $05
-	smpsNoteFill        $00
+	smpsSetvoice	$03
+	smpsNoteFill	$00
 	dc.b	nA2, $68
 	dc.b	nA3, $58
-	smpsNoteFill        $09
+	smpsNoteFill	$09
 
 BGM_LimitEgg_Loop00:
-	dc.b	nA2, $04, nRst, $28, nA2, $04, nA2, nA2, $02, nRst, $2A, nA2
-	dc.b	$04, nRst, $28, nA2, $04, nA2, nA2, nRst, $18, nA4, $04, nA4
+	dc.b	nA2, $2C, $04, $04, $2C
+	dc.b	nA2, $2C, $04, $04, $1C, nA4, $04, nA4
 	dc.b	nA5, nA5
-	smpsLoop            $00, $04, BGM_LimitEgg_Loop00
+	smpsLoop	0, 4, BGM_LimitEgg_Loop00
 
 BGM_LimitEgg_Loop01:
 	dc.b	nA2, $04, nRst, nA4, nA4, nA4, nA4
-	smpsLoop            $00, $04, BGM_LimitEgg_Loop01
-	dc.b	nA2, $04, nRst, $28, nA2, $04, nA2, nA2, nA4, nA4, nA5, nA5
+	smpsLoop	0, 4, BGM_LimitEgg_Loop01
+	dc.b	nA2, $2C, $04, nA2, nA2, nA4, nA4, nA5, nA5
 	dc.b	nRst, $08, nA4, $04, nA4, nA5, nA5
-	smpsJump            BGM_LimitEgg_FM5
+	smpsJump	BGM_LimitEgg_FM5
 
 ; FM2 Data
 BGM_LimitEgg_FM2:
-	smpsSetvoice        $01
-	smpsNoteFill        $0A
+	smpsSetvoice	$01
+	smpsNoteFill	$0A
 
 BGM_LimitEgg_Jump00:
 	dc.b	nA1, $04, nA2, nA2, nA1, nA2, nA1, nA2, nA1, nA2, nA2, nA1
 	dc.b	nA2, nA1, $02, nRst, $2E, nA1, $04, nA2, nA2, nA1, nA2, nA1
 	dc.b	nA2, nA1, nA3, nA3, nA1, nA3, nA1, $02, nRst, $2E
-	smpsJump            BGM_LimitEgg_Jump00
+	smpsJump	BGM_LimitEgg_Jump00
 
 ; PSG2 Data
 BGM_LimitEgg_PSG2:
-	smpsAlterNote       $FE
+	smpsAlterNote	$FE
 
 ; PSG1 Data
 BGM_LimitEgg_PSG1:
 	dc.b	nRst, $18
-	smpsLoop            $00, $18, BGM_LimitEgg_PSG1
+	smpsLoop	0, 24, BGM_LimitEgg_PSG1
 
 BGM_LimitEgg_Loop09:
-	dc.b	nG2, $18
-	dc.b	nG2, $48
-	dc.b	nG2, $08
+	dc.b	nG2, $68
 	dc.b	nG3, $58
-	smpsLoop            $00, $02, BGM_LimitEgg_Loop09
+	smpsLoop	0, 2, BGM_LimitEgg_Loop09
 
 BGM_LimitEgg_Loop0A:
-	dc.b	nG2, $08
-	dc.b	nG2, $10
-	dc.b	$08
+	dc.b	nG2, $20
 	dc.b	nG3, $10
-	smpsLoop            $00, $02, BGM_LimitEgg_Loop0A
-	dc.b	nG2, $08
-	dc.b	nG2, $58
-	smpsJump            BGM_LimitEgg_PSG1
+	smpsLoop	0, 2, BGM_LimitEgg_Loop0A
+	dc.b	nG2, $60
+	smpsJump	BGM_LimitEgg_PSG1
 
 ; DAC Data
 BGM_LimitEgg_DAC:
-	dc.b	nRst, $30, dSnare, $04, dSnare, dSnare, $10, dSnare, $04, dSnare, dSnare, $10
-	dc.b	nRst, $30, dSnare, $04, dSnare, nRst, dSnare, nRst, dSnare, dSnare, dSnare, dSnare
-	dc.b	$08, dSnare, dSnare, $08, dSnare, $04, $04, $08, dSnare, dSnare, dSnare, dSnare
-	dc.b	dSnare, $0C, $04, dSnare, $08, dSnare, dSnare, dSnare, dSnare, $04, $08, $04
-	dc.b	dSnare, $08, dSnare, dSnare, dSnare, dSnare, $04, $08, $04, dSnare, $08, dSnare
-	dc.b	$04, $04, $08, dSnare, dSnare, dSnare, dSnare, dSnare, $04, $08, $04, dSnare
-	dc.b	$08, dSnare, dSnare, dSnare, $08, dSnare, $04, $04, $08, dSnare, dSnare, dSnare
-	dc.b	dSnare, dSnare, $04, $08, $04, dSnare, $08, dSnare, dSnare, dSnare, dSnare, $04
-	dc.b	dSnare, dSnare, dSnare, dSnare, $08, dSnare, dSnare, dSnare, dSnare, $04, $08, $04
-	dc.b	dSnare, $08, dSnare, dSnare, dSnare, $08, dSnare, $04, $04, $08, dSnare, dSnare
-	dc.b	dSnare, dSnare, dSnare, $04, $08, $04, dSnare, $08, dSnare, dSnare, dSnare, $04
-	dc.b	dSnare, dSnare, dSnare, dSnare, dSnare, dSnare, $08, dSnare, dSnare, dSnare, dSnare, $04
-	dc.b	$08, $04, dSnare, $08, dSnare, dSnare, dSnare, dSnare, $04, $04, $08, dSnare
-	dc.b	dSnare, dSnare, dSnare, dSnare, $04, $08, $04, dSnare, $08, dSnare, dSnare, dSnare
-	dc.b	$04, dSnare, $08, $04, dSnare, dSnare, dSnare, $08, dSnare, dSnare, dSnare, $08
-	dc.b	dSnare, $04, dSnare, dSnare, dSnare, dSnare, $08, dSnare, dSnare, dSnare, $08, dSnare
-	dc.b	$04, dSnare, dSnare, dSnare, dSnare, $08, dSnare, dSnare, dSnare, $08, dSnare, $04
-	dc.b	$08, $04, dSnare, $04, dSnare, $08, $04, dSnare, dSnare, dSnare, $04, dSnare
-	dc.b	$08, $04, dSnare, dSnare
-	smpsJump            BGM_LimitEgg_DAC
+	dc.b	nRst, $30, dSnare, $04, $04, $10, $04, $04, $10
+	dc.b	nRst, $30, dSnare, $04, $08, $08, $04, $04, $04, $08, $08
+	dc.b	$08, $04, $04, $08, $08, $08, $08, $08, $0C, $04, $08, $08, $08
+	dc.b	$08, $04, $08, $04, $08, $08, $08, $08, $04, $08, $04, $08, $04, $04, $08
+	smpsCall	BGM_LimitEgg_DAC_Call00
+	smpsCall	BGM_LimitEgg_DAC_Call01
+	dc.b	$08, $04, $04, $04, $04
+	smpsCall	BGM_LimitEgg_DAC_Call00
+	smpsCall	BGM_LimitEgg_DAC_Call01
+	dc.b	$04, $04, $04, $04, $04, $04
+	smpsCall	BGM_LimitEgg_DAC_Call00
+	smpsCall	BGM_LimitEgg_DAC_Call01
+	dc.b	$04, $08, $04, $04, $04
+	dc.b	$08, $08, $08, $08, $04, $04, $04, $04, $08, $08, $08, $08, $04, $04, $04, $04
+	dc.b	$08, $08, $08, $08, $04, $08, $04, $04, $08, $04, $04, $04, $04, $08, $04, $04, $04
+	smpsJump	BGM_LimitEgg_DAC
+
+BGM_LimitEgg_DAC_Call01:
+	dc.b	$08, $04, $04, $08
+
+BGM_LimitEgg_DAC_Call00:
+	dc.b	$08, $08, $08, $08, $04, $08, $04, $08, $08, $08
+	smpsReturn
 
 ; PSG3 Data
 BGM_LimitEgg_PSG3:
-	smpsPSGform         $E7
+	smpsPSGform	$E7
 
 BGM_LimitEgg_Jump02:
-	dc.b	nRst, $08, nMaxPSG, nMaxPSG, nRst, $08, nMaxPSG, nMaxPSG, nRst, $30, nRst, $08
-	dc.b	nMaxPSG, nMaxPSG, nRst, $08, nMaxPSG, nMaxPSG, nRst, $30
+	dc.b	nRst, $08, nMaxPSG, nMaxPSG, nRst, $08, nMaxPSG, nMaxPSG, nRst, $30
+	smpsLoop	0, 2, BGM_LimitEgg_Jump02
 
 BGM_LimitEgg_Loop08:
 	dc.b	nRst, $18
-	smpsLoop            $00, $28, BGM_LimitEgg_Loop08
-	smpsJump            BGM_LimitEgg_Jump02
+	smpsLoop	$00, 40, BGM_LimitEgg_Loop08
+	smpsJump	BGM_LimitEgg_Jump02
 
 BGM_LimitEgg_Voices:
 ;	Voice $00
@@ -254,42 +271,6 @@ BGM_LimitEgg_Voices:
 	smpsVcTotalLevel    $00, $2E, $0E, $1A
 
 ;	Voice $03
-;	$06
-;	$01, $33, $72, $31, 	$0A, $8C, $4C, $52, 	$00, $00, $00, $00
-;	$01, $00, $01, $00, 	$0F, $0F, $2F, $0F, 	$4D, $87, $80, $91
-	smpsVcAlgorithm     $06
-	smpsVcFeedback      $00
-	smpsVcUnusedBits    $00
-	smpsVcDetune        $03, $07, $03, $00
-	smpsVcCoarseFreq    $01, $02, $03, $01
-	smpsVcRateScale     $01, $01, $02, $00
-	smpsVcAttackRate    $12, $0C, $0C, $0A
-	smpsVcAmpMod        $00, $00, $00, $00
-	smpsVcDecayRate1    $00, $00, $00, $00
-	smpsVcDecayRate2    $00, $01, $00, $01
-	smpsVcDecayLevel    $00, $02, $00, $00
-	smpsVcReleaseRate   $0F, $0F, $0F, $0F
-	smpsVcTotalLevel    $11, $00, $07, $4D
-
-;	Voice $04
-;	$3A
-;	$01, $02, $01, $01, 	$10, $0E, $14, $10, 	$0C, $0E, $0E, $0E
-;	$00, $00, $00, $00, 	$0F, $FF, $7F, $1F, 	$1C, $28, $31, $80
-	smpsVcAlgorithm     $02
-	smpsVcFeedback      $07
-	smpsVcUnusedBits    $00
-	smpsVcDetune        $00, $00, $00, $00
-	smpsVcCoarseFreq    $01, $01, $02, $01
-	smpsVcRateScale     $00, $00, $00, $00
-	smpsVcAttackRate    $10, $14, $0E, $10
-	smpsVcAmpMod        $00, $00, $00, $00
-	smpsVcDecayRate1    $0E, $0E, $0E, $0C
-	smpsVcDecayRate2    $00, $00, $00, $00
-	smpsVcDecayLevel    $01, $07, $0F, $00
-	smpsVcReleaseRate   $0F, $0F, $0F, $0F
-	smpsVcTotalLevel    $00, $31, $28, $1C
-
-;	Voice $05
 ;	$39
 ;	$02, $01, $02, $01, 	$1F, $1F, $1F, $1F, 	$00, $00, $00, $00
 ;	$00, $00, $00, $00, 	$0F, $0F, $0F, $0F, 	$1B, $32, $28, $80
