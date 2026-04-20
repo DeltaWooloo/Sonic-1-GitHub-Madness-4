9d95a4a7756b2ee3481bce5b8c88f0f49e7d01b2
Giovanni Battaglia <iamgiovanni.gb@gmail.com>
2026-04-21 00:26:23 +0200

    new sprites + they are somewhat centered now
    apologies for importing on "reduce mappings" mode and for wasting ROM space lmao

--- dotgen/knight/[DPLC] The Roaring Knight.asm
+++ dotgen/knight/[DPLC] The Roaring Knight.asm
@@ -21,6 +21,36 @@ DPLCs_Knight: mappingsTable
 	mappingsTableEntry.w	DPLCs_Knight_19
 	mappingsTableEntry.w	DPLCs_Knight_20
 	mappingsTableEntry.w	DPLCs_Knight_21
+	mappingsTableEntry.w	DPLCs_Knight_22
+	mappingsTableEntry.w	DPLCs_Knight_23
+	mappingsTableEntry.w	DPLCs_Knight_24
+	mappingsTableEntry.w	DPLCs_Knight_25
+	mappingsTableEntry.w	DPLCs_Knight_26
+	mappingsTableEntry.w	DPLCs_Knight_27
+	mappingsTableEntry.w	DPLCs_Knight_28
+	mappingsTableEntry.w	DPLCs_Knight_29
+	mappingsTableEntry.w	DPLCs_Knight_30
+	mappingsTableEntry.w	DPLCs_Knight_31
+	mappingsTableEntry.w	DPLCs_Knight_32
+	mappingsTableEntry.w	DPLCs_Knight_33
+	mappingsTableEntry.w	DPLCs_Knight_34
+	mappingsTableEntry.w	DPLCs_Knight_35
+	mappingsTableEntry.w	DPLCs_Knight_36
+	mappingsTableEntry.w	DPLCs_Knight_37
+	mappingsTableEntry.w	DPLCs_Knight_38
+	mappingsTableEntry.w	DPLCs_Knight_39
+	mappingsTableEntry.w	DPLCs_Knight_40
+	mappingsTableEntry.w	DPLCs_Knight_41
+	mappingsTableEntry.w	DPLCs_Knight_42
+	mappingsTableEntry.w	DPLCs_Knight_43
+	mappingsTableEntry.w	DPLCs_Knight_44
+	mappingsTableEntry.w	DPLCs_Knight_45
+	mappingsTableEntry.w	DPLCs_Knight_46
+	mappingsTableEntry.w	DPLCs_Knight_47
+	mappingsTableEntry.w	DPLCs_Knight_48
+	mappingsTableEntry.w	DPLCs_Knight_49
+	mappingsTableEntry.w	DPLCs_Knight_50
+	mappingsTableEntry.w	DPLCs_Knight_51
 
 DPLCs_Knight_0:	dplcHeader
 DPLCs_Knight_0_End
@@ -40,107 +70,289 @@ DPLCs_Knight_2_End
 DPLCs_Knight_3:	dplcHeader
  dplcEntry $10, $4F
  dplcEntry 1, $5F
- dplcEntry 1, $63
- dplcEntry $F, $68
- dplcEntry 2, $7A
- dplcEntry 3, $77
- dplcEntry 2, $7C
+ dplcEntry 1, $60
+ dplcEntry $F, $61
+ dplcEntry 2, $73
+ dplcEntry 3, $70
+ dplcEntry 2, $75
 DPLCs_Knight_3_End
 
 DPLCs_Knight_4:	dplcHeader
- dplcEntry $10, $7E
- dplcEntry $10, $8E
- dplcEntry $C, $9E
+ dplcEntry $10, $77
+ dplcEntry $10, $87
+ dplcEntry $C, $97
 DPLCs_Knight_4_End
 
 DPLCs_Knight_5:	dplcHeader
- dplcEntry $10, $AA
- dplcEntry $10, $BA
- dplcEntry 9, $CA
+ dplcEntry $10, $A3
+ dplcEntry $10, $B3
+ dplcEntry 9, $C3
 DPLCs_Knight_5_End
 
 DPLCs_Knight_6:	dplcHeader
- dplcEntry $10, $D3
- dplcEntry $10, $E3
- dplcEntry $F, $F3
+ dplcEntry $10, $CC
+ dplcEntry $10, $DC
+ dplcEntry $F, $EC
 DPLCs_Knight_6_End
 
 DPLCs_Knight_7:	dplcHeader
- dplcEntry 4, $102
+ dplcEntry 4, $FB
 DPLCs_Knight_7_End
 
 DPLCs_Knight_8:	dplcHeader
- dplcEntry 4, $106
+ dplcEntry 4, $FF
 DPLCs_Knight_8_End
 
 DPLCs_Knight_9:	dplcHeader
- dplcEntry 4, $10A
+ dplcEntry 4, $103
 DPLCs_Knight_9_End
 
 DPLCs_Knight_10:	dplcHeader
- dplcEntry 4, $10E
+ dplcEntry 4, $107
 DPLCs_Knight_10_End
 
 DPLCs_Knight_11:	dplcHeader
- dplcEntry 4, $112
+ dplcEntry 4, $10B
 DPLCs_Knight_11_End
 
 DPLCs_Knight_12:	dplcHeader
- dplcEntry 3, $116
+ dplcEntry 3, $10F
 DPLCs_Knight_12_End
 
 DPLCs_Knight_13:	dplcHeader
- dplcEntry $A, $119
+ dplcEntry $A, $112
 DPLCs_Knight_13_End
 
 DPLCs_Knight_14:	dplcHeader
- dplcEntry $10, $123
- dplcEntry $C, $133
+ dplcEntry $10, $11C
+ dplcEntry $C, $12C
 DPLCs_Knight_14_End
 
 DPLCs_Knight_15:	dplcHeader
- dplcEntry $10, $13F
- dplcEntry $10, $14F
- dplcEntry $10, $15F
- dplcEntry 1, $16F
+ dplcEntry $10, $138
+ dplcEntry $10, $148
+ dplcEntry $10, $158
+ dplcEntry 1, $168
 DPLCs_Knight_15_End
 
 DPLCs_Knight_16:	dplcHeader
- dplcEntry $10, $170
- dplcEntry $10, $180
- dplcEntry $10, $190
- dplcEntry $A, $1A0
+ dplcEntry $10, $169
+ dplcEntry $10, $179
+ dplcEntry $10, $189
+ dplcEntry $A, $199
 DPLCs_Knight_16_End
 
 DPLCs_Knight_17:	dplcHeader
- dplcEntry $10, $1AA
- dplcEntry $10, $1BA
- dplcEntry $10, $1CA
- dplcEntry $E, $1DA
+ dplcEntry $10, $1A3
+ dplcEntry $10, $1B3
+ dplcEntry $10, $1C3
+ dplcEntry $E, $1D3
 DPLCs_Knight_17_End
 
 DPLCs_Knight_18:	dplcHeader
- dplcEntry $10, $1E8
- dplcEntry $10, $1F8
- dplcEntry $10, $208
- dplcEntry $C, $218
+ dplcEntry $10, $1E1
+ dplcEntry $10, $1F1
+ dplcEntry $10, $201
+ dplcEntry $C, $211
 DPLCs_Knight_18_End
 
 DPLCs_Knight_19:	dplcHeader
- dplcEntry $10, $224
- dplcEntry $A, $234
+ dplcEntry $10, $21D
+ dplcEntry $A, $22D
 DPLCs_Knight_19_End
 
 DPLCs_Knight_20:	dplcHeader
- dplcEntry $10, $23E
- dplcEntry $10, $24E
- dplcEntry $F, $25E
+ dplcEntry $10, $237
+ dplcEntry $10, $247
+ dplcEntry $F, $257
 DPLCs_Knight_20_End
 
 DPLCs_Knight_21:	dplcHeader
- dplcEntry $10, $26D
- dplcEntry $10, $27D
- dplcEntry $F, $28D
+ dplcEntry $10, $266
+ dplcEntry $10, $276
+ dplcEntry $F, $286
 DPLCs_Knight_21_End
+
+DPLCs_Knight_22:	dplcHeader
+ dplcEntry $10, $295
+ dplcEntry $10, $2A5
+ dplcEntry $E, $2B5
+DPLCs_Knight_22_End
+
+DPLCs_Knight_23:	dplcHeader
+ dplcEntry $10, $2C3
+ dplcEntry $10, $2D3
+ dplcEntry $10, $2E3
+ dplcEntry 5, $2F3
+DPLCs_Knight_23_End
+
+DPLCs_Knight_24:	dplcHeader
+ dplcEntry $10, $2F8
+ dplcEntry $10, $308
+ dplcEntry $10, $318
+ dplcEntry 8, $328
+DPLCs_Knight_24_End
+
+DPLCs_Knight_25:	dplcHeader
+ dplcEntry $10, $330
+ dplcEntry $10, $340
+ dplcEntry $10, $350
+ dplcEntry 5, $360
+DPLCs_Knight_25_End
+
+DPLCs_Knight_26:	dplcHeader
+ dplcEntry $10, $365
+ dplcEntry $10, $375
+ dplcEntry $10, $385
+ dplcEntry 2, $395
+DPLCs_Knight_26_End
+
+DPLCs_Knight_27:	dplcHeader
+ dplcEntry $10, $397
+ dplcEntry $10, $3A7
+ dplcEntry $10, $3B7
+ dplcEntry $E, $3C7
+DPLCs_Knight_27_End
+
+DPLCs_Knight_28:	dplcHeader
+ dplcEntry $10, $3D5
+ dplcEntry $10, $3E5
+ dplcEntry $10, $3F5
+ dplcEntry $C, $405
+DPLCs_Knight_28_End
+
+DPLCs_Knight_29:	dplcHeader
+ dplcEntry $10, $411
+ dplcEntry $10, $421
+ dplcEntry $10, $431
+ dplcEntry $10, $441
+ dplcEntry $10, $451
+ dplcEntry $10, $461
+ dplcEntry $10, $471
+ dplcEntry 5, $481
+DPLCs_Knight_29_End
+
+DPLCs_Knight_30:	dplcHeader
+ dplcEntry $10, $486
+ dplcEntry $10, $496
+ dplcEntry $10, $4A6
+ dplcEntry $10, $4B6
+ dplcEntry 4, $4C6
+DPLCs_Knight_30_End
+
+DPLCs_Knight_31:	dplcHeader
+ dplcEntry $10, $4CA
+ dplcEntry $10, $4DA
+ dplcEntry $10, $4EA
+ dplcEntry $10, $4FA
+ dplcEntry 4, $50A
+DPLCs_Knight_31_End
+
+DPLCs_Knight_32:	dplcHeader
+ dplcEntry $10, $50E
+ dplcEntry $10, $51E
+ dplcEntry $D, $52E
+DPLCs_Knight_32_End
+
+DPLCs_Knight_33:	dplcHeader
+ dplcEntry $10, $53B
+ dplcEntry $10, $54B
+ dplcEntry $C, $55B
+DPLCs_Knight_33_End
+
+DPLCs_Knight_34:	dplcHeader
+ dplcEntry $10, $567
+ dplcEntry $10, $577
+ dplcEntry $E, $587
+DPLCs_Knight_34_End
+
+DPLCs_Knight_35:	dplcHeader
+ dplcEntry $10, $595
+ dplcEntry $F, $5A5
+DPLCs_Knight_35_End
+
+DPLCs_Knight_36:	dplcHeader
+ dplcEntry $10, $5B4
+ dplcEntry 5, $5C4
+DPLCs_Knight_36_End
+
+DPLCs_Knight_37:	dplcHeader
+ dplcEntry $C, $5C9
+DPLCs_Knight_37_End
+
+DPLCs_Knight_38:	dplcHeader
+ dplcEntry 4, $5D5
+DPLCs_Knight_38_End
+
+DPLCs_Knight_39:	dplcHeader
+ dplcEntry 4, $5D9
+DPLCs_Knight_39_End
+
+DPLCs_Knight_40:	dplcHeader
+ dplcEntry 4, $5DD
+DPLCs_Knight_40_End
+
+DPLCs_Knight_41:	dplcHeader
+ dplcEntry $A, $5E1
+DPLCs_Knight_41_End
+
+DPLCs_Knight_42:	dplcHeader
+ dplcEntry $10, $5EB
+ dplcEntry 6, $5FB
+DPLCs_Knight_42_End
+
+DPLCs_Knight_43:	dplcHeader
+ dplcEntry $10, $601
+ dplcEntry $10, $611
+ dplcEntry $A, $621
+DPLCs_Knight_43_End
+
+DPLCs_Knight_44:	dplcHeader
+ dplcEntry $10, $62B
+ dplcEntry $10, $63B
+ dplcEntry $B, $64B
+DPLCs_Knight_44_End
+
+DPLCs_Knight_45:	dplcHeader
+ dplcEntry $10, $656
+ dplcEntry $10, $666
+ dplcEntry $C, $676
+DPLCs_Knight_45_End
+
+DPLCs_Knight_46:	dplcHeader
+ dplcEntry $10, $682
+ dplcEntry $10, $692
+ dplcEntry $D, $6A2
+DPLCs_Knight_46_End
+
+DPLCs_Knight_47:	dplcHeader
+ dplcEntry $10, $6AF
+ dplcEntry $10, $6BF
+ dplcEntry 6, $6CF
+DPLCs_Knight_47_End
+
+DPLCs_Knight_48:	dplcHeader
+ dplcEntry $10, $6D5
+ dplcEntry $10, $6E5
+ dplcEntry 7, $6F5
+DPLCs_Knight_48_End
+
+DPLCs_Knight_49:	dplcHeader
+ dplcEntry $10, $6FC
+ dplcEntry $10, $70C
+ dplcEntry $A, $71C
+DPLCs_Knight_49_End
+
+DPLCs_Knight_50:	dplcHeader
+ dplcEntry $10, $726
+ dplcEntry $10, $736
+ dplcEntry $C, $746
+DPLCs_Knight_50_End
+
+DPLCs_Knight_51:	dplcHeader
+ dplcEntry $10, $752
+ dplcEntry $10, $762
+ dplcEntry $D, $772
+DPLCs_Knight_51_End
 
 	even
--- dotgen/knight/[MAPPINGS] The Roaring Knight.asm
+++ dotgen/knight/[MAPPINGS] The Roaring Knight.asm
@@ -21,6 +21,36 @@ Maps_Knight: mappingsTable
 	mappingsTableEntry.w	Maps_Knight_19
 	mappingsTableEntry.w	Maps_Knight_20
 	mappingsTableEntry.w	Maps_Knight_21
+	mappingsTableEntry.w	Maps_Knight_22
+	mappingsTableEntry.w	Maps_Knight_23
+	mappingsTableEntry.w	Maps_Knight_24
+	mappingsTableEntry.w	Maps_Knight_25
+	mappingsTableEntry.w	Maps_Knight_26
+	mappingsTableEntry.w	Maps_Knight_27
+	mappingsTableEntry.w	Maps_Knight_28
+	mappingsTableEntry.w	Maps_Knight_29
+	mappingsTableEntry.w	Maps_Knight_30
+	mappingsTableEntry.w	Maps_Knight_31
+	mappingsTableEntry.w	Maps_Knight_32
+	mappingsTableEntry.w	Maps_Knight_33
+	mappingsTableEntry.w	Maps_Knight_34
+	mappingsTableEntry.w	Maps_Knight_35
+	mappingsTableEntry.w	Maps_Knight_36
+	mappingsTableEntry.w	Maps_Knight_37
+	mappingsTableEntry.w	Maps_Knight_38
+	mappingsTableEntry.w	Maps_Knight_39
+	mappingsTableEntry.w	Maps_Knight_40
+	mappingsTableEntry.w	Maps_Knight_41
+	mappingsTableEntry.w	Maps_Knight_42
+	mappingsTableEntry.w	Maps_Knight_43
+	mappingsTableEntry.w	Maps_Knight_44
+	mappingsTableEntry.w	Maps_Knight_45
+	mappingsTableEntry.w	Maps_Knight_46
+	mappingsTableEntry.w	Maps_Knight_47
+	mappingsTableEntry.w	Maps_Knight_48
+	mappingsTableEntry.w	Maps_Knight_49
+	mappingsTableEntry.w	Maps_Knight_50
+	mappingsTableEntry.w	Maps_Knight_51
 
 Maps_Knight_0:	spriteHeader
 Maps_Knight_0_End
@@ -245,5 +275,305 @@ Maps_Knight_21:	spriteHeader
  spritePiece -3, $F, 1, 1, $2D, 0, 0, 0, 0
  spritePiece -$B, $1F, 1, 1, $2E, 0, 0, 0, 0
 Maps_Knight_21_End
+
+Maps_Knight_22:	spriteHeader
+ spritePiece -$B, -$22, 4, 4, 0, 0, 0, 0, 0
+ spritePiece -$13, -$12, 1, 4, $10, 0, 0, 0, 0
+ spritePiece -$2B, -$A, 3, 2, $14, 0, 0, 0, 0
+ spritePiece $15, -$A, 1, 3, $1A, 0, 0, 0, 0
+ spritePiece -$B, -2, 3, 3, $1D, 0, 0, 0, 0
+ spritePiece $D, -2, 1, 2, $26, 0, 0, 0, 0
+ spritePiece -$23, 6, 2, 2, $28, 0, 0, 0, 0
+ spritePiece -3, $16, 1, 2, $2C, 0, 0, 0, 0
+Maps_Knight_22_End
+
+Maps_Knight_23:	spriteHeader
+ spritePiece -1, -$26, 3, 4, 0, 0, 0, 0, 0
+ spritePiece -$11, -$1E, 2, 3, $C, 0, 0, 0, 0
+ spritePiece -$29, -$E, 3, 4, $12, 0, 0, 0, 0
+ spritePiece -$31, -6, 1, 2, $1E, 0, 0, 0, 0
+ spritePiece -9, -6, 4, 4, $20, 0, 0, 0, 0
+ spritePiece -$11, 2, 1, 2, $30, 0, 0, 0, 0
+ spritePiece $17, $A, 1, 1, $32, 0, 0, 0, 0
+ spritePiece -9, $1A, 2, 1, $33, 0, 0, 0, 0
+Maps_Knight_23_End
+
+Maps_Knight_24:	spriteHeader
+ spritePiece -8, -$24, 4, 3, 0, 0, 0, 0, 0
+ spritePiece -$18, -$1C, 2, 3, $C, 0, 0, 0, 0
+ spritePiece -$38, -$14, 4, 2, $12, 0, 0, 0, 0
+ spritePiece -8, -$C, 3, 4, $1A, 0, 0, 0, 0
+ spritePiece -$30, -4, 2, 3, $26, 0, 0, 0, 0
+ spritePiece -$20, 4, 3, 2, $2C, 0, 0, 0, 0
+ spritePiece $10, $C, 2, 1, $32, 0, 0, 0, 0
+ spritePiece -8, $14, 2, 2, $34, 0, 0, 0, 0
+Maps_Knight_24_End
+
+Maps_Knight_25:	spriteHeader
+ spritePiece -8, -$24, 4, 3, 0, 0, 0, 0, 0
+ spritePiece -$18, -$1C, 2, 3, $C, 0, 0, 0, 0
+ spritePiece -$38, -$14, 4, 1, $12, 0, 0, 0, 0
+ spritePiece -$30, -$C, 3, 1, $16, 0, 0, 0, 0
+ spritePiece -8, -$C, 3, 4, $19, 0, 0, 0, 0
+ spritePiece -$30, 4, 4, 2, $25, 0, 0, 0, 0
+ spritePiece -$10, 4, 1, 2, $2D, 0, 0, 0, 0
+ spritePiece $10, $C, 2, 1, $2F, 0, 0, 0, 0
+ spritePiece -8, $14, 2, 2, $31, 0, 0, 0, 0
+Maps_Knight_25_End
+
+Maps_Knight_26:	spriteHeader
+ spritePiece -$29, 0, 3, 2, 0, 0, 0, 0, 0
+ spritePiece -$21, -$18, 4, 1, 6, 0, 0, 0, 0
+ spritePiece -$19, -$10, 4, 2, $A, 0, 0, 0, 0
+ spritePiece -$11, -$28, 4, 2, $12, 0, 0, 0, 0
+ spritePiece -$11, 0, 3, 1, $1A, 0, 0, 0, 0
+ spritePiece -9, 8, 2, 3, $1D, 0, 0, 0, 0
+ spritePiece -1, -$18, 4, 1, $23, 0, 0, 0, 0
+ spritePiece -1, $20, 1, 1, $27, 0, 0, 0, 0
+ spritePiece 7, -$10, 2, 2, $28, 0, 0, 0, 0
+ spritePiece $F, 0, 3, 2, $2C, 0, 0, 0, 0
+Maps_Knight_26_End
+
+Maps_Knight_27:	spriteHeader
+ spritePiece -1, -$46, 3, 1, 0, 0, 0, 0, 0
+ spritePiece -9, -$3E, 3, 1, 3, 0, 0, 0, 0
+ spritePiece -$11, -$36, 3, 4, 6, 0, 0, 0, 0
+ spritePiece -$19, -$2E, 1, 4, $12, 0, 0, 0, 0
+ spritePiece -$21, -$26, 1, 3, $16, 0, 0, 0, 0
+ spritePiece 7, -$26, 2, 1, $19, 0, 0, 0, 0
+ spritePiece -$29, -$1E, 1, 2, $1B, 0, 0, 0, 0
+ spritePiece 7, -$1E, 1, 4, $1D, 0, 0, 0, 0
+ spritePiece -$11, -$16, 3, 4, $21, 0, 0, 0, 0
+ spritePiece $F, -$16, 2, 2, $2D, 0, 0, 0, 0
+ spritePiece -$19, -$E, 1, 3, $31, 0, 0, 0, 0
+ spritePiece -9, $A, 3, 3, $34, 0, 0, 0, 0
+ spritePiece 7, $22, 1, 1, $3D, 0, 0, 0, 0
+Maps_Knight_27_End
+
+Maps_Knight_28:	spriteHeader
+ spritePiece -$27, -$27, 4, 3, 0, 0, 0, 0, 0
+ spritePiece -$17, -$2F, 3, 1, $C, 0, 0, 0, 0
+ spritePiece -$17, -$F, 4, 3, $F, 0, 0, 0, 0
+ spritePiece -$F, -$37, 3, 1, $1B, 0, 0, 0, 0
+ spritePiece -$F, 9, 3, 1, $1E, 0, 0, 0, 0
+ spritePiece -7, -$3F, 4, 1, $21, 0, 0, 0, 0
+ spritePiece -7, -$27, 3, 3, $25, 0, 0, 0, 0
+ spritePiece -7, $11, 3, 2, $2E, 0, 0, 0, 0
+ spritePiece 1, -$47, 3, 1, $34, 0, 0, 0, 0
+ spritePiece 1, $21, 2, 1, $37, 0, 0, 0, 0
+ spritePiece 9, -$F, 3, 1, $39, 0, 0, 0, 0
+Maps_Knight_28_End
+
+Maps_Knight_29:	spriteHeader
+ spritePiece -$43, -$51, 2, 3, 0, 0, 0, 0, 0
+ spritePiece -$3B, -$39, 2, 4, 6, 0, 0, 0, 0
+ spritePiece -$2B, -$31, 2, 4, $E, 0, 0, 0, 0
+ spritePiece -$1B, -$29, 4, 4, $16, 0, 0, 0, 0
+ spritePiece 5, -$29, 2, 4, $26, 0, 0, 0, 0
+ spritePiece -$33, -$19, 1, 2, $2E, 0, 0, 0, 0
+ spritePiece $15, -$19, 1, 2, $30, 0, 0, 0, 0
+ spritePiece -$2B, -$11, 2, 3, $32, 0, 0, 0, 0
+ spritePiece -$1B, -9, 4, 4, $38, 0, 0, 0, 0
+ spritePiece 5, -9, 2, 4, $48, 0, 0, 0, 0
+ spritePiece -$23, 7, 1, 1, $50, 0, 0, 0, 0
+ spritePiece -$13, $17, 4, 1, $51, 0, 0, 0, 0
+ spritePiece $D, $17, 2, 4, $55, 0, 0, 0, 0
+ spritePiece -$B, $1F, 3, 1, $5D, 0, 0, 0, 0
+ spritePiece $1D, $1F, 2, 4, $60, 0, 0, 0, 0
+ spritePiece -3, $27, 2, 1, $68, 0, 0, 0, 0
+ spritePiece $2D, $27, 3, 3, $6A, 0, 0, 0, 0
+ spritePiece $45, $2F, 1, 2, $73, 0, 0, 0, 0
+Maps_Knight_29_End
+
+Maps_Knight_30:	spriteHeader
+ spritePiece -$26, -1, 2, 2, 0, 0, 0, 0, 0
+ spritePiece -$1E, -$19, 4, 3, 4, 0, 0, 0, 0
+ spritePiece -$E, -$29, 4, 2, $10, 0, 0, 0, 0
+ spritePiece -$E, 7, 3, 3, $18, 0, 0, 0, 0
+ spritePiece -6, -1, 4, 1, $21, 0, 0, 0, 0
+ spritePiece -6, $1F, 2, 3, $25, 0, 0, 0, 0
+ spritePiece 2, -$19, 3, 3, $2B, 0, 0, 0, 0
+ spritePiece $12, 7, 2, 2, $34, 0, 0, 0, 0
+ spritePiece $1A, $17, 3, 1, $38, 0, 0, 0, 0
+ spritePiece $22, $F, 1, 1, $3B, 0, 0, 0, 0
+ spritePiece $22, $1F, 4, 1, $3C, 0, 0, 0, 0
+ spritePiece $2A, $27, 4, 1, $40, 0, 0, 0, 0
+Maps_Knight_30_End
+
+Maps_Knight_31:	spriteHeader
+ spritePiece -$25, 1, 2, 2, 0, 0, 0, 0, 0
+ spritePiece -$1D, -$17, 4, 3, 4, 0, 0, 0, 0
+ spritePiece -$D, -$27, 4, 2, $10, 0, 0, 0, 0
+ spritePiece -$D, 1, 3, 4, $18, 0, 0, 0, 0
+ spritePiece -5, $21, 2, 2, $24, 0, 0, 0, 0
+ spritePiece -5, $31, 1, 1, $28, 0, 0, 0, 0
+ spritePiece 3, -$17, 2, 3, $29, 0, 0, 0, 0
+ spritePiece $B, 1, 3, 2, $2F, 0, 0, 0, 0
+ spritePiece $13, -$1F, 1, 2, $35, 0, 0, 0, 0
+ spritePiece $13, $11, 3, 1, $37, 0, 0, 0, 0
+ spritePiece $1B, $19, 3, 1, $3A, 0, 0, 0, 0
+ spritePiece $23, $21, 4, 1, $3D, 0, 0, 0, 0
+ spritePiece $2B, $29, 3, 1, $41, 0, 0, 0, 0
+Maps_Knight_31_End
+
+Maps_Knight_32:	spriteHeader
+ spritePiece -$A, -$22, 4, 4, 0, 0, 0, 0, 0
+ spritePiece -$12, -$12, 1, 4, $10, 0, 0, 0, 0
+ spritePiece -$2A, -$A, 3, 2, $14, 0, 0, 0, 0
+ spritePiece $16, -$A, 1, 3, $1A, 0, 0, 0, 0
+ spritePiece -$A, -2, 4, 2, $1D, 0, 0, 0, 0
+ spritePiece -$1A, 6, 1, 2, $25, 0, 0, 0, 0
+ spritePiece -$12, $E, 4, 1, $27, 0, 0, 0, 0
+ spritePiece -2, $16, 1, 2, $2B, 0, 0, 0, 0
+Maps_Knight_32_End
+
+Maps_Knight_33:	spriteHeader
+ spritePiece -$F, -$27, 3, 4, 0, 0, 0, 0, 0
+ spritePiece 9, -$17, 2, 4, $C, 0, 0, 0, 0
+ spritePiece -$1F, -$F, 2, 4, $14, 0, 0, 0, 0
+ spritePiece -$27, -7, 1, 3, $1C, 0, 0, 0, 0
+ spritePiece -$F, -7, 3, 3, $1F, 0, 0, 0, 0
+ spritePiece -7, $11, 2, 2, $28, 0, 0, 0, 0
+Maps_Knight_33_End
+
+Maps_Knight_34:	spriteHeader
+ spritePiece -$19, -$21, 4, 4, 0, 0, 0, 0, 0
+ spritePiece -$29, -$11, 2, 2, $10, 0, 0, 0, 0
+ spritePiece 7, -$11, 2, 4, $14, 0, 0, 0, 0
+ spritePiece $17, -9, 1, 3, $1C, 0, 0, 0, 0
+ spritePiece -$19, -1, 4, 3, $1F, 0, 0, 0, 0
+ spritePiece -$29, $F, 2, 1, $2B, 0, 0, 0, 0
+ spritePiece -1, $17, 1, 1, $2D, 0, 0, 0, 0
+Maps_Knight_34_End
+
+Maps_Knight_35:	spriteHeader
+ spritePiece -$20, -$F, 4, 2, 0, 0, 0, 0, 0
+ spritePiece -$10, -$17, 3, 1, 8, 0, 0, 0, 0
+ spritePiece -$10, 1, 4, 2, $B, 0, 0, 0, 0
+ spritePiece -8, $11, 3, 1, $13, 0, 0, 0, 0
+ spritePiece 0, -$1F, 1, 1, $16, 0, 0, 0, 0
+ spritePiece 0, -$F, 4, 2, $17, 0, 0, 0, 0
+Maps_Knight_35_End
+
+Maps_Knight_36:	spriteHeader
+ spritePiece -$12, -9, 4, 4, 0, 0, 0, 0, 0
+ spritePiece -$A, -$11, 4, 1, $10, 0, 0, 0, 0
+ spritePiece $E, 7, 1, 1, $14, 0, 0, 0, 0
+Maps_Knight_36_End
+
+Maps_Knight_37:	spriteHeader
+ spritePiece -$10, -$C, 4, 3, 0, 0, 0, 0, 0
+Maps_Knight_37_End
+
+Maps_Knight_38:	spriteHeader
+ spritePiece -8, -8, 2, 2, 0, 0, 0, 0, 0
+Maps_Knight_38_End
+
+Maps_Knight_39:	spriteHeader
+ spritePiece -8, -8, 2, 2, 0, 0, 0, 0, 0
+Maps_Knight_39_End
+
+Maps_Knight_40:	spriteHeader
+ spritePiece -8, -8, 2, 2, 0, 0, 0, 0, 0
+Maps_Knight_40_End
+
+Maps_Knight_41:	spriteHeader
+ spritePiece -5, -$13, 2, 4, 0, 0, 0, 0, 0
+ spritePiece -$D, -$B, 1, 2, 8, 0, 0, 0, 0
+Maps_Knight_41_End
+
+Maps_Knight_42:	spriteHeader
+ spritePiece -$C, -$18, 3, 4, 0, 0, 0, 0, 0
+ spritePiece -4, -$28, 2, 2, $C, 0, 0, 0, 0
+ spritePiece $C, -$20, 2, 2, $10, 0, 0, 0, 0
+ spritePiece $1C, -$18, 1, 2, $14, 0, 0, 0, 0
+Maps_Knight_42_End
+
+Maps_Knight_43:	spriteHeader
+ spritePiece -$F, -$E, 2, 3, 0, 0, 0, 0, 0
+ spritePiece -7, -$26, 4, 3, 6, 0, 0, 0, 0
+ spritePiece 1, 2, 1, 1, $12, 0, 0, 0, 0
+ spritePiece $11, 2, 4, 1, $13, 0, 0, 0, 0
+ spritePiece $19, -$2E, 2, 4, $17, 0, 0, 0, 0
+ spritePiece $19, -$E, 4, 2, $1F, 0, 0, 0, 0
+ spritePiece $29, -$26, 1, 3, $27, 0, 0, 0, 0
+Maps_Knight_43_End
+
+Maps_Knight_44:	spriteHeader
+ spritePiece -8, -$27, 2, 2, 0, 0, 0, 0, 0
+ spritePiece 8, -$27, 3, 1, 4, 0, 0, 0, 0
+ spritePiece -$10, -$1F, 1, 2, 7, 0, 0, 0, 0
+ spritePiece $10, -$1F, 4, 4, 9, 0, 0, 0, 0
+ spritePiece $30, -$17, 1, 4, $19, 0, 0, 0, 0
+ spritePiece -8, -7, 3, 2, $1D, 0, 0, 0, 0
+ spritePiece $10, 1, 4, 2, $23, 0, 0, 0, 0
+Maps_Knight_44_End
+
+Maps_Knight_45:	spriteHeader
+ spritePiece $1B, -$21, 2, 4, 0, 0, 0, 0, 0
+ spritePiece $2B, -$19, 1, 4, 8, 0, 0, 0, 0
+ spritePiece -$1D, -9, 4, 3, $C, 0, 0, 0, 0
+ spritePiece 3, -9, 3, 4, $18, 0, 0, 0, 0
+ spritePiece -$2D, -1, 2, 1, $24, 0, 0, 0, 0
+ spritePiece $1B, -1, 2, 2, $26, 0, 0, 0, 0
+ spritePiece -$D, $F, 2, 1, $2A, 0, 0, 0, 0
+Maps_Knight_45_End
+
+Maps_Knight_46:	spriteHeader
+ spritePiece -$C, -$C, 4, 4, 0, 0, 0, 0, 0
+ spritePiece $14, -$C, 4, 3, $10, 0, 0, 0, 0
+ spritePiece -$34, -4, 4, 2, $1C, 0, 0, 0, 0
+ spritePiece -$14, -4, 1, 3, $24, 0, 0, 0, 0
+ spritePiece $14, $C, 2, 1, $27, 0, 0, 0, 0
+ spritePiece -$C, $14, 4, 1, $29, 0, 0, 0, 0
+Maps_Knight_46_End
+
+Maps_Knight_47:	spriteHeader
+ spritePiece -$30, -4, 4, 2, 0, 0, 0, 0, 0
+ spritePiece -$18, $C, 4, 1, 8, 0, 0, 0, 0
+ spritePiece -$10, -$C, 3, 3, $C, 0, 0, 0, 0
+ spritePiece -$10, $14, 4, 1, $15, 0, 0, 0, 0
+ spritePiece 8, -4, 3, 3, $19, 0, 0, 0, 0
+ spritePiece $20, -4, 2, 2, $22, 0, 0, 0, 0
+Maps_Knight_47_End
+
+Maps_Knight_48:	spriteHeader
+ spritePiece -$30, -4, 4, 2, 0, 0, 0, 0, 0
+ spritePiece -$18, $C, 4, 1, 8, 0, 0, 0, 0
+ spritePiece -$10, -$C, 3, 3, $C, 0, 0, 0, 0
+ spritePiece -$10, $14, 4, 1, $15, 0, 0, 0, 0
+ spritePiece 8, -4, 4, 3, $19, 0, 0, 0, 0
+ spritePiece $28, -4, 1, 2, $25, 0, 0, 0, 0
+Maps_Knight_48_End
+
+Maps_Knight_49:	spriteHeader
+ spritePiece -$30, -4, 4, 2, 0, 0, 0, 0, 0
+ spritePiece -$18, $C, 4, 2, 8, 0, 0, 0, 0
+ spritePiece -$10, -$C, 3, 3, $10, 0, 0, 0, 0
+ spritePiece -8, $1C, 3, 1, $19, 0, 0, 0, 0
+ spritePiece 8, -4, 3, 3, $1C, 0, 0, 0, 0
+ spritePiece 8, $14, 1, 1, $25, 0, 0, 0, 0
+ spritePiece $20, -4, 2, 2, $26, 0, 0, 0, 0
+Maps_Knight_49_End
+
+Maps_Knight_50:	spriteHeader
+ spritePiece -$30, -4, 4, 2, 0, 0, 0, 0, 0
+ spritePiece -$18, $C, 4, 2, 8, 0, 0, 0, 0
+ spritePiece -$10, -$C, 3, 3, $10, 0, 0, 0, 0
+ spritePiece 0, $1C, 2, 1, $19, 0, 0, 0, 0
+ spritePiece 8, -4, 4, 3, $1B, 0, 0, 0, 0
+ spritePiece 8, $14, 3, 1, $27, 0, 0, 0, 0
+ spritePiece $28, -4, 1, 2, $2A, 0, 0, 0, 0
+Maps_Knight_50_End
+
+Maps_Knight_51:	spriteHeader
+ spritePiece -$1C, -$A, 4, 4, 0, 0, 0, 0, 0
+ spritePiece 4, -$A, 2, 4, $10, 0, 0, 0, 0
+ spritePiece -$34, -2, 3, 2, $18, 0, 0, 0, 0
+ spritePiece $14, -2, 3, 3, $1E, 0, 0, 0, 0
+ spritePiece $2C, -2, 1, 1, $27, 0, 0, 0, 0
+ spritePiece -$C, $16, 3, 1, $28, 0, 0, 0, 0
+ spritePiece $14, $16, 2, 1, $2B, 0, 0, 0, 0
+Maps_Knight_51_End
 
 	even
