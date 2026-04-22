e414242de241a98d72e4aeac49d10fbf866d7d80
Tamk1s <tamkis314@gmail.com>
2026-04-22 11:30:47 -0400

    Updated all blue-screen/text-based BSODs to use IBM PC font

--- Assets/Levels/Win/winDie/bsodSCD/build.bat
+++ Assets/Levels/Win/winDie/bsodSCD/build.bat
@@ -7,8 +7,4 @@ copy /y "bsodcd_ntsc-u_mapFG.eni" "..\..\..\..\..\_gamemode\winBSOD\bsod_scd_nts
 copy /y "bsodcd_ntsc-j_mapFG.eni" "..\..\..\..\..\_gamemode\winBSOD\bsod_scd_ntscj-mapFG.eni"
 copy /y "bsodcd_pal-e_mapFG.eni" "..\..\..\..\..\_gamemode\winBSOD\bsod_scd_pale-mapFG.eni"
 copy /y "bsodcd_pal-a_mapFG.eni" "..\..\..\..\..\_gamemode\winBSOD\bsod_scd_pala-mapFG.eni"
-pause
-
-
-
-
+REM pause
