d30e949c60cb29b4b4c32a56eab030d213ea8d57
DaxKatter2117 <daxternator2117@gmail.com>
2026-04-23 22:29:43 +0800

    Fix broken Windows Zone Scrolling

--- _inc/LevelSizeLoad & BgScrollSpeed (JP1).asm
+++ _inc/LevelSizeLoad & BgScrollSpeed (JP1).asm
@@ -186,7 +186,7 @@ BgScroll_Index:	dc.w BgScroll_GHZ-BgScroll_Index, BgScroll_LZ-BgScroll_Index
 		dc.w BgScroll_MZ-BgScroll_Index, BgScroll_SLZ-BgScroll_Index
 		dc.w BgScroll_SYZ-BgScroll_Index, BgScroll_SBZ-BgScroll_Index
 		dc.w BgScroll_End-BgScroll_Index, BgScroll_GHZ-BgScroll_Index
-		dc.w BgScroll_ITBZ-BgScroll_Index, BgScroll_Default-BgScroll_Index
+		dc.w BgScroll_Default-BgScroll_Index, BgScroll_ITBZ-BgScroll_Index
 		dc.w BgScroll_NGZ-BgScroll_Index, BgScroll_NGZ-BgScroll_Index
 		dc.w BgScroll_Default-BgScroll_Index, BgScroll_Default-BgScroll_Index
 		dc.w BgScroll_ARZ-BgScroll_Index
