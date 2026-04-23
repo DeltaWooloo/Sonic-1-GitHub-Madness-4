244846d7452c0ca1054852d519e67e0557b35e50
DaxKatter2117 <daxternator2117@gmail.com>
2026-04-23 23:41:15 +0800

    Update Music Order

--- Constants.asm
+++ Constants.asm
@@ -242,7 +242,7 @@ af2ndRoutine:	equ $FA	; increment 2nd routine counter
 	nextenum bgm_MWaterS=bgm__First		; Orange World	(Act 1)
 	nextenum bgm_OrangeSong			; 		(Act 2)
 	nextenum bgm_GreenHills			; 		(Act 3)
-	nextenum bgm_Dungeon3			; Azure Rainforest
+						; Wario Hallway
 	nextenum bgm_LosTontos			; Alberta Canada	(Act 1)
 	nextenum bgm_Area5			; 			(Act 2)
 	nextenum bgm_Easton			; 			(Act 3)
@@ -257,7 +257,7 @@ af2ndRoutine:	equ $FA	; increment 2nd routine counter
 	nextenum bgm_Cheetah			; 		(Act 2)
 	nextenum bgm_REMansion			; 		(Act 3)
 	nextenum bgm_ColdBrew			; Cold Brew	(Act 1)
-	nextenum bgm_ValSDST1			; Cold Brew	(Act 2)
+	nextenum bgm_ValSDST1			; 		(Act 2)
 	nextenum bgm_UNOwenWasHer		; Windows	(Act 1)
 	nextenum bgm_Passport			; 		(Act 2)
 	nextenum bgm_VirusAlert			; 		(Act 3)
@@ -267,6 +267,7 @@ af2ndRoutine:	equ $FA	; increment 2nd routine counter
 	nextenum bgm_GHZ			; Nogales Zone
 	nextenum bgm_TF2			; Bluescapes	(Act 1)		; Dax: Interesting song choice for this :/
 	nextenum bgm_Blue			;		(Act 2)
+	nextenum bgm_Dungeon3			; Azure Rainforest
 	nextenum bgm_Final			; Final Zone
 
 	; Boss BGM
@@ -294,7 +295,6 @@ af2ndRoutine:	equ $FA	; increment 2nd routine counter
 	nextenum bgm_NewBarkTown		; Debug Menu
 	nextenum bgm_Memories			; Character/Difficulty Select
 	nextenum bgm_DoleAttack			; Intro Cutscene
-	nextenum bgm_Dingaling			; RPG Battle (might change this)
 	nextenum bgm_Spoopy			; Sans Death Screen
 	nextenum bgm_Continue			; Continue Screen
 	nextenum bgm_Ending			; Ending A
@@ -352,8 +352,10 @@ af2ndRoutine:	equ $FA	; increment 2nd routine counter
 	nextenum bgm_BossaNova
 	nextenum bgm_BatMan
 	nextenum bgm_Hidden
+	nextenum bgm_SadMac
 
 	; Unused Full/Looping BGM
+	nextenum bgm_Dingaling
 	nextenum bgm_BonusEight
 	nextenum bgm_Pac2
 	nextenum bgm_SS
@@ -393,7 +395,6 @@ af2ndRoutine:	equ $FA	; increment 2nd routine counter
 	nextenum bgm_SonicToole
 	nextenum bgm_Folgers
 	nextenum bgm_Ding
-	nextenum bgm_SadMac
 	nextenum bgm_Win2K
 	nextenum bgm_CrazyMario
 	; Keep this last
