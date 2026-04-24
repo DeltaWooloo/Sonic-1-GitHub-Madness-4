260b12afe0729ecb0c2f32154521f08a353c8391
ConiNight <129713751+ConiNight@users.noreply.github.com>
2026-04-24 23:11:50 +0300

    dummy out S1 credits

--- _inc/MoveSonicInDemo.asm
+++ _inc/MoveSonicInDemo.asm
@@ -13,25 +13,25 @@ MoveSonicInDemo:
 
 ; This is an unused subroutine for recording a demo
 
-DemoRecorder:
-		; This was likely intended for a deveveloper cartridge that used RAM instead of ROM.
-		lea	(EndOfRom).l,a1 ; Write past the end of the ROM.
-		move.w	(v_btnpushtime1).w,d0
-		adda.w	d0,a1
-		move.b	(v_jpadhold1).w,d0
-		cmp.b	(a1),d0
-		bne.s	.next
-		addq.b	#1,1(a1)
-		cmpi.b	#$FF,1(a1)
-		beq.s	.next
-		rts
-
-.next:
-		move.b	d0,2(a1)
-		move.b	#0,3(a1)
-		addq.w	#2,(v_btnpushtime1).w
-		andi.w	#$3FF,(v_btnpushtime1).w
-		rts
+;DemoRecorder:
+;		; This was likely intended for a deveveloper cartridge that used RAM instead of ROM.
+;		lea	(EndOfRom).l,a1 ; Write past the end of the ROM.
+;		move.w	(v_btnpushtime1).w,d0
+;		adda.w	d0,a1
+;		move.b	(v_jpadhold1).w,d0
+;		cmp.b	(a1),d0
+;		bne.s	.next
+;		addq.b	#1,1(a1)
+;		cmpi.b	#$FF,1(a1)
+;		beq.s	.next
+;		rts
+;
+;.next:
+;		move.b	d0,2(a1)
+;		move.b	#0,3(a1)
+;		addq.w	#2,(v_btnpushtime1).w
+;		andi.w	#$3FF,(v_btnpushtime1).w
+;		rts
 ; ===========================================================================
 
 MDemo_On:
@@ -39,9 +39,9 @@ MDemo_On:
 		bne.s	.timerend
 		move.w	#3,(f_restart).w			; trigger a fadeout to black
 		move.b	#id_Sega,(v_gamemode).w			; go to Sega screen
-		tst.w	(f_demo).w				; check if this demo is for the credits
-		bpl.s	.timerend
-		move.b	#id_Credits,(v_gamemode).w		; go to credits again during a credits demo
+;		tst.w	(f_demo).w				; check if this demo is for the credits
+;		bpl.s	.timerend
+;		move.b	#id_Credits,(v_gamemode).w		; go to credits again during a credits demo
 .timerend:
 		tst.b	(v_jpadhold1).w	; is start button pressed?
 		bpl.s	.dontquit	; if not, branch
@@ -55,15 +55,15 @@ MDemo_On:
 		move.b	(v_zone).w,d0
 		lsl.w	#2,d0
 		movea.l	(a1,d0.w),a1	; fetch address for demo data
-		tst.w	(f_demo).w	; is this an ending sequence demo?
-		bpl.s	.notcredits	; if not, branch
-		lea	(DemoEndDataPtr).l,a1
-		move.w	(v_creditsnum).w,d0
-		subq.w	#1,d0
-		lsl.w	#2,d0
-		movea.l	(a1,d0.w),a1	; fetch address for credits demo
-
-.notcredits:
+;		tst.w	(f_demo).w	; is this an ending sequence demo?
+;		bpl.s	.notcredits	; if not, branch
+;		lea	(DemoEndDataPtr).l,a1
+;		move.w	(v_creditsnum).w,d0
+;		subq.w	#1,d0
+;		lsl.w	#2,d0
+;		movea.l	(a1,d0.w),a1	; fetch address for credits demo
+;
+;.notcredits:
 		move.w	(v_btnpushtime1).w,d0
 		adda.w	d0,a1
 		move.b	(a1),d0
@@ -98,14 +98,14 @@ DemoDataPtr:	dc.l Demo_GHZ		; demos run after the title screen
 		dc.l Demo_BREW
 		dc.l Demo_BREW
 
-DemoEndDataPtr:	dc.l Demo_EndGHZ1	; demos run during the credits
-		dc.l Demo_EndMZ
-		dc.l Demo_EndSYZ
-		dc.l Demo_EndLZ
-		dc.l Demo_EndSLZ
-		dc.l Demo_EndSBZ1
-		dc.l Demo_EndSBZ2
-		dc.l Demo_EndGHZ2
+;DemoEndDataPtr:	dc.l Demo_EndGHZ1	; demos run during the credits
+;		dc.l Demo_EndMZ
+;		dc.l Demo_EndSYZ
+;		dc.l Demo_EndLZ
+;		dc.l Demo_EndSLZ
+;		dc.l Demo_EndSBZ1
+;		dc.l Demo_EndSBZ2
+;		dc.l Demo_EndGHZ2
 
 ; Johnson demo data is present here. It involves Funny Butthole slowly running
 ; right, Fatting once, then Bowser Farting at full speed for a few seconds.
