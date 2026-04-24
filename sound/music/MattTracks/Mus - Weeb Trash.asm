5b74e87ce2e1d0fc44d5c7a5be86e5dfe99e5846
DaxKatter2117 <daxternator2117@gmail.com>
2026-04-25 06:08:00 +0800

    Whoops, I made an error

--- sound/music/MattTracks/Mus - Weeb Trash.asm
+++ sound/music/MattTracks/Mus - Weeb Trash.asm
@@ -873,9 +873,9 @@ BGM_WeebTrash_PSG3_02_0_64_0B:
 	smpsPSGAlterVol	$FF
 
 BGM_WeebTrash_PSG3_02_Loop:
-	dc.b	nMaxPSG1-24, $01
+	dc.b	nMaxPSG1-28, $01
 	smpsAlterPitch	$01
-	smpsLoop	0, 24, BGM_WeebTrash_PSG3_02_Loop
+	smpsLoop	0, 28, BGM_WeebTrash_PSG3_02_Loop
 	smpsAlterPitch	-25
 	dc.b	nMaxPSG2, $28
 	smpsReturn
