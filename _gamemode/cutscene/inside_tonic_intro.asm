ba7fd0c48d8ab46087d759334372fb9abdd3d718
KatKuriN <69405557+KatKuriN@users.noreply.github.com>
2026-04-27 22:39:38 -0500

    inside tonic intro stuff

---
+++ _gamemode/cutscene/inside_tonic_intro.asm
@@ -0,0 +1,27 @@
+Cutscene_TonicIntro:
+	moveq   #0,d0
+	move.b	subscene.w,d0
+	add.w	d0,d0
+	add.w	d0,d0
+	jsr	.Index(pc,d0.w)
+	move.w	cameraAPosX.w, v_scrposx_vdp.w
+	move.w	cameraAPosY.w, v_scrposy_vdp.w
+	move.w	cameraBPosX.w, v_bgscrposx_vdp.w
+	move.w	cameraBPosY.w, v_bgscrposy_vdp.w
+	move.w	cameraZPosX.w, v_bg3scrposx_vdp.w
+	move.w	cameraZPosY.w, v_bg3scrposy_vdp.w
+	add.w	#1,v_framecount.w
+	rts
+.Index:
+	bra.w	TonicIntro_FadeIn
+	rts	; put more routines here
+	nop
+
+TonicIntro_FadeIn:
+	addq.b	#1,subscene.w
+	jmp	PalFadeIn
+
+
+Str_TonicIntro1:
+	dc.b	"Teeth Tonic is a Colorado Cracker",0
+	even
