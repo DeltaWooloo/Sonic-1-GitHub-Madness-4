55774d66134b20a88e394761c5c8ff921ae3b0f7
KatKuriN <69405557+KatKuriN@users.noreply.github.com>
2026-05-04 20:57:19 -0500

    fiix the boss idfk

--- _maps/Eggman.asm
+++ _maps/Eggman.asm
@@ -1,92 +1,64 @@
-Map_5cdd: mappingsTable
-	mappingsTableEntry.w	Map_5cdd_0
-	mappingsTableEntry.w	Map_5cdd_1
-	mappingsTableEntry.w	Map_5cdd_2
-	mappingsTableEntry.w	Map_5cdd_3
-	mappingsTableEntry.w	Map_5cdd_4
-	mappingsTableEntry.w	Map_5cdd_5
-	mappingsTableEntry.w	Map_5cdd_6
-	mappingsTableEntry.w	Map_5cdd_7
-	mappingsTableEntry.w	Map_5cdd_8
-	mappingsTableEntry.w	Map_5cdd_9
-	mappingsTableEntry.w	Map_5cdd_10
-	mappingsTableEntry.w	Map_5cdd_11
-	mappingsTableEntry.w	Map_5cdd_12
-
-Map_5cdd_0:	spriteHeader
- spritePiece $C, -$14, 2, 2, $C, 0, 0, 0, 0
- spritePiece -$1C, -4, 4, 3, $10, 0, 0, 1, 0
- spritePiece 4, -4, 4, 3, $1C, 0, 0, 1, 0
- spritePiece -$14, $14, 4, 1, $28, 0, 0, 1, 0
- spritePiece $C, $14, 1, 1, $2C, 0, 0, 1, 0
- spritePiece -$14, -$14, 1, 1, $35, 0, 0, 0, 0
- spritePiece -$1C, -$C, 1, 1, $B, 0, 0, 0, 0
- spritePiece -$14, -$C, 1, 1, 7, 0, 0, 0, 0
- spritePiece -$1C, -$14, 1, 1, 3, 0, 0, 0, 0
-Map_5cdd_0_End
-
-Map_5cdd_1:	spriteHeader
- spritePiece -$D, -$1C, 3, 1, 0, 0, 0, 0, 0
- spritePiece -$D, -$14, 3, 1, 4, 0, 0, 0, 0
- spritePiece -$D, -$C, 3, 1, 8, 0, 0, 0, 0
-Map_5cdd_1_End
-
-Map_5cdd_2:	spriteHeader
- spritePiece -$D, -$1C, 3, 1, 0, 0, 0, 0, 0
- spritePiece -$D, -$14, 3, 1, 4, 0, 0, 0, 0
- spritePiece -$D, -$C, 3, 1, 8, 0, 0, 0, 0
-Map_5cdd_2_End
-
-Map_5cdd_3:	spriteHeader
- spritePiece -$D, -$1C, 3, 3, $34, 0, 0, 0, 0
-Map_5cdd_3_End
-
-Map_5cdd_4:	spriteHeader
- spritePiece -$D, -$14, 3, 2, $3D, 0, 0, 0, 0
- spritePiece -$D, -$1B, 1, 1, $34, 0, 0, 0, 0
- spritePiece -5, -$1B, 1, 1, $37, 0, 0, 0, 0
- spritePiece 3, -$1B, 1, 1, $3A, 0, 0, 0, 0
-Map_5cdd_4_End
-
-Map_5cdd_5:	spriteHeader
- spritePiece -5, -$1F, 2, 4, $43, 0, 0, 0, 0
- spritePiece -$C, -$1F, 1, 4, $47, 1, 0, 0, 0
- spritePiece -$D, -$1C, 3, 3, $34, 0, 0, 0, 0
-Map_5cdd_5_End
-
-Map_5cdd_6:	spriteHeader
- spritePiece -$D, -$1C, 3, 1, 0, 0, 0, 0, 0
- spritePiece -$D, -$14, 3, 1, 4, 0, 0, 0, 0
- spritePiece -$D, -$C, 3, 1, 8, 0, 0, 0, 0
- spritePiece $A, -$20, 2, 1, $64, 0, 0, 0, 0
-Map_5cdd_6_End
-
-Map_5cdd_7:	spriteHeader
- spritePiece $22, 4, 2, 2, $2D, 0, 0, 0, 0
-Map_5cdd_7_End
-
-Map_5cdd_8:	spriteHeader
- spritePiece -5, -$1F, 2, 4, $43, 0, 0, 0, 0
- spritePiece -$C, -$1F, 1, 4, $47, 1, 0, 0, 0
- spritePiece -$D, -$1C, 3, 3, $34, 0, 0, 0, 0
-Map_5cdd_8_End
-
-Map_5cdd_9:	spriteHeader
- spritePiece $22, 4, 1, 2, $31, 0, 0, 0, 0
- spritePiece $2A, 4, 1, 1, $33, 0, 0, 0, 0
-Map_5cdd_9_End
-
-Map_5cdd_10:	spriteHeader
-Map_5cdd_10_End
-
-Map_5cdd_11:	spriteHeader
- spritePiece $22, -8, 3, 4, $12D, 0, 0, 0, 0
- spritePiece $3A, 0, 1, 2, $139, 0, 0, 0, 0
-Map_5cdd_11_End
-
-Map_5cdd_12:	spriteHeader
- spritePiece $22, 0, 3, 1, $12A, 0, 0, 0, 0
- spritePiece $22, 8, 3, 1, $12A, 0, 1, 0, 0
-Map_5cdd_12_End
-
+Map_9e7c:
+Map_9e7c_0: 	dc.w Map_9e7c_1A-Map_9e7c
+Map_9e7c_2: 	dc.w Map_9e7c_43-Map_9e7c
+Map_9e7c_4: 	dc.w Map_9e7c_53-Map_9e7c
+Map_9e7c_6: 	dc.w Map_9e7c_63-Map_9e7c
+Map_9e7c_8: 	dc.w Map_9e7c_69-Map_9e7c
+Map_9e7c_A: 	dc.w Map_9e7c_7E-Map_9e7c
+Map_9e7c_C: 	dc.w Map_9e7c_8E-Map_9e7c
+Map_9e7c_E: 	dc.w Map_9e7c_A3-Map_9e7c
+Map_9e7c_10: 	dc.w Map_9e7c_B3-Map_9e7c
+Map_9e7c_12: 	dc.w Map_9e7c_B9-Map_9e7c
+Map_9e7c_14: 	dc.w Map_9e7c_C4-Map_9e7c
+Map_9e7c_16: 	dc.w Map_9e7c_C5-Map_9e7c
+Map_9e7c_18: 	dc.w Map_9e7c_D0-Map_9e7c
+Map_9e7c_1A: 	dc.b $8
+	dc.b $EC, $5, $0, $C, $C
+	dc.b $FC, $E, $20, $10, $E4
+	dc.b $FC, $E, $20, $1C, $4
+	dc.b $14, $C, $20, $28, $EC
+	dc.b $14, $0, $20, $2C, $C
+	dc.b $F4, $0, $0, $B, $E4
+	dc.b $EC, $1, $0, $5A, $EC
+	dc.b $EC, $0, $0, $57, $E4
+Map_9e7c_43: 	dc.b $3
+	dc.b $E4, $8, $0, $0, $F4
+	dc.b $EC, $8, $0, $4, $F4
+	dc.b $F4, $8, $0, $8, $F4
+Map_9e7c_53: 	dc.b $3
+	dc.b $E4, $8, $0, $0, $F4
+	dc.b $EC, $8, $0, $4, $F4
+	dc.b $F4, $8, $0, $8, $F4
+Map_9e7c_63: 	dc.b $1
+	dc.b $E4, $A, $0, $34, $F4
+Map_9e7c_69: 	dc.b $4
+	dc.b $EC, $9, $0, $3D, $F4
+	dc.b $E5, $0, $0, $34, $F4
+	dc.b $E5, $0, $0, $37, $FC
+	dc.b $E5, $0, $0, $3A, $4
+Map_9e7c_7E: 	dc.b $3
+	dc.b $E1, $7, $0, $43, $FC
+	dc.b $E1, $3, $8, $47, $F5
+	dc.b $E4, $A, $0, $34, $F4
+Map_9e7c_8E: 	dc.b $4
+	dc.b $E4, $8, $0, $0, $F4
+	dc.b $EC, $8, $0, $4, $F4
+	dc.b $F4, $8, $0, $8, $F4
+	dc.b $E0, $4, $0, $64, $B
+Map_9e7c_A3: 	dc.b $3
+	dc.b $E1, $7, $0, $43, $FC
+	dc.b $E1, $3, $8, $47, $F5
+	dc.b $E4, $A, $0, $34, $F4
+Map_9e7c_B3: 	dc.b $1
+	dc.b $4, $5, $0, $2D, $22
+Map_9e7c_B9: 	dc.b $2
+	dc.b $4, $1, $0, $31, $22
+	dc.b $4, $0, $0, $33, $2A
+Map_9e7c_C4: 	dc.b $0
+Map_9e7c_C5: 	dc.b $2
+	dc.b $0, $8, $1, $2A, $22
+	dc.b $8, $8, $11, $2A, $22
+Map_9e7c_D0: 	dc.b $2
+	dc.b $F8, $B, $1, $2D, $22
+	dc.b $0, $1, $1, $39, $3A
 	even
