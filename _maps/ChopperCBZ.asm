b84ef5ebe523e91460624022cccfd4db654ef6fb
ConiNight <129713751+ConiNight@users.noreply.github.com>
2026-05-19 22:09:31 +0300

    stupiud sexy flanders

--- _maps/ChopperCBZ.asm
+++ _maps/ChopperCBZ.asm
@@ -1,17 +1,17 @@
-Map_aad6: mappingsTable
-	mappingsTableEntry.w	Map_aad6_0
-	mappingsTableEntry.w	Map_aad6_1
+Map_6bf6: mappingsTable
+	mappingsTableEntry.w	Map_6bf6_0
+	mappingsTableEntry.w	Map_6bf6_1
 
-Map_aad6_0:	spriteHeader
- spritePiece -$10, -$14, 2, 4, 0, 0, 0, 0, 0
- spritePiece 0, -$14, 2, 4, 0, 1, 0, 0, 0
- spritePiece -$10, $C, 4, 1, 8, 0, 0, 0, 0
-Map_aad6_0_End
+Map_6bf6_0:	spriteHeader
+ spritePiece -$10, -$1C, 4, 3, 0, 0, 0, 0, 0
+ spritePiece -$10, -4, 3, 2, $C, 0, 0, 0, 0
+ spritePiece -$10, $C, 3, 2, $12, 0, 0, 0, 0
+Map_6bf6_0_End
 
-Map_aad6_1:	spriteHeader
- spritePiece -$10, -$14, 2, 4, $C, 0, 0, 0, 0
- spritePiece 0, -$14, 2, 4, $C, 1, 0, 0, 0
- spritePiece -$10, $C, 4, 1, $14, 0, 0, 0, 0
-Map_aad6_1_End
+Map_6bf6_1:	spriteHeader
+ spritePiece -$F, -$1C, 4, 3, 0, 0, 0, 0, 0
+ spritePiece -$11, -4, 3, 2, $C, 0, 0, 0, 0
+ spritePiece -$10, $C, 3, 2, $12, 0, 0, 0, 0
+Map_6bf6_1_End
 
 	even
